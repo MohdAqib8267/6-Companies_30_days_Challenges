467e45e89be606d652eed565b6f25df2c85ae78a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-06 02:24:08 +0530

    Time: 9 ms (41.23%), Space: 6.7 MB (44.76%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+//     Imagine that the secret number is "1807" and your friend makes a guess of "7810". In this case, there is one bull because the digit 8 is in the correct position. There are also three cows because the digits 7, 1, and 0 are present in the secret number, but are not in the correct position. The hint for this guess would be "1A3B", which means that there is 1 bull and 3 cows.
+
+// Now let's consider another example where the secret number is "1123" and the guess is "0111". In this case, there is one bull because the digit 1 is in the correct position. There is also one cow because the digit 1 is present in the secret number, but is not in the correct position. The hint for this guess would be "1A1B", which means that there is 1 bull and 1 cow.
     string getHint(string secret, string guess) {
         unordered_map<int,int>mp;
         int bull=0, cow=0;
