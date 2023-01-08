99f14d51759fd2b5a47d12718d07a517b9b5ee49
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-08 17:34:24 +0530

    Time: 285 ms (62.80%), Space: 84.4 MB (14.93%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     //idea
-//     First we will store all the projects in projects vector as pairs {Profit(i), Capital(i)};
+// First we will store all the projects in projects vector as pairs {Profit(i), Capital(i)};
 // Now we will sort all the projects according to its capital value.
 // Now we will fetch all the projects that we can perform for our own capital value.
 // After fetching all these projects sotre their profit value in Max Heap (maxProfit in below code).
 // Now check if the Heap is Empty or not, if its not empty then take the top value of the heap (indicates that we chose max profit value from given projects, think greedy here).
 // Now perform step 3, k times because we want to perform exactly k projects to gain max profit.
+    
+    //w=3, k==4
+    //after sorting
+    //[{4,1},{3,2},{6,4},{5,5},{4,9},{23,13},{22,15},{40,39},{49,41}]
+    //ans=58  (DRY RUN)
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         vector<pair<int,int>>v;
         for(int i=0;i<profits.size();i++){
