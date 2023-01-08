e7606cef86a55255d157d1b0a58a812cf9c8bfcc
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-08 21:03:49 +0530

    Time: 217 ms (60.43%), Space: 55.9 MB (15.11%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long low=0,high=0;
+        long long sum=0;
+        long long ans=0;
+        for(auto i:batteries){
+            sum+=i;
+        }
+        high=sum;
+        sort(batteries.begin(),batteries.end());//binary search
+        if(n==1){
+            return sum;
+        }
+        while(low<high){
+            long long mid=low+(high-low)/2;
+            long long cnt=0;
+            long long temp=0;
+            for(auto i:batteries){
+                temp+=i;
+                if(temp>=mid){
+                    temp=temp-mid;
+                    cnt++;
+                }
+            }
+            if(cnt>=n){
+                //mtlab hum mid ko zyada computers me distrubute kr paa rhey hau
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return ans;
+    }
+};
