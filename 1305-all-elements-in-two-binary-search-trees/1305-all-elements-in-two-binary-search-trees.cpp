044a84c38028fd7f583d642b3d4b87235b96807a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-09 16:27:41 +0530

    Time: 178 ms (66.50%), Space: 84.6 MB (65.27%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        //method 1
+        //store in vector v1,preorder of  tree1
+        //store in vector v1,preorder of  tree2
+        //sort v1 and return 
+        //motheod 2 (stack and binary search)
+         stack<TreeNode *> st1, st2;
+        vector<int> res;
+        
+        while(root1 || root2 || !st1.empty() || !st2.empty()){
+            while(root1){
+                st1.push(root1);
+                root1 = root1->left;
+            }
+            while(root2){
+                st2.push(root2);
+                root2 = root2->left;
+            }
+            if(st2.empty() || (!st1.empty() && st1.top()->val <= st2.top()->val)){
+                root1 = st1.top();
+                st1.pop();
+                res.push_back(root1->val);
+                root1 = root1->right;
+            }
+            else{
+                root2 = st2.top();
+                st2.pop();
+                res.push_back(root2->val);
+                root2 = root2->right;
+            }
+        }
+        return res;
+    }
+};
