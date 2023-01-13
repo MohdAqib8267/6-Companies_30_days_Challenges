c88dbe9da7416a3b8bd4cf85005304e1faee463e
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-13 16:41:02 +0530

    Time: 438 ms (80.93%), Space: 181.3 MB (65.77%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:    
+//     Intuition
+// Classic dfs problem on the tree.
+
+
+// Explanation
+// dfs(i) return the the longest path starting at the node i.
+
+// dfs on each node i, and for each node i,
+// we iterate on all its children,
+// and find all the path startint at the node i.
+// The 2 longest path can make up the longest path in subtree of node i,
+// we update the global result res,
+// and return the longest path length as result for dfs.
+     int longestPath(vector<int>& parent, string s) {
+        int n = s.size(), res = 0;
+        vector<vector<int>> children(n, vector<int>());
+        for (int i = 1; i < n; ++i)
+            children[parent[i]].push_back(i);
+        dfs(children, s, res, 0);
+        return res;
+    }
+
     int dfs(vector<vector<int>>& children, string& s, int& res, int i) {
         int big1 = 0, big2 = 0;
-        if(children[i].size()==0){
-            return 1;
-        }
         for (int& j : children[i]) {
             int cur = dfs(children, s, res, j);
             if (s[i] == s[j]) continue;
@@ -13,15 +32,6 @@ public:
         }
         res = max(res, big1 + big2 + 1);
         return big1 + 1;
-    }
-   int longestPath(vector<int>& parent, string s) {
-        int n = s.size(), res = 1;
-       
-        vector<vector<int>> children(n, vector<int>());
-        for (int i = 1; i < n; ++i)
-            children[parent[i]].push_back(i);
-        dfs(children, s, res, 0);
-        return res;
     }
 
 
