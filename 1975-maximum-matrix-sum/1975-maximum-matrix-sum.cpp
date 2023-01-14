3d2b8608d9187e8d546b063dd88e3b308b1d43f1
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-14 22:12:48 +0530

    Time: 218 ms (40.84%), Space: 35.2 MB (73.71%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    /*
+	If number of -ve numbers are even, it means you can ALWAYS shift -1 in a
+	fashion that you can cancel out the negative signs and make the numbers positive.
+	So in that case, total sum (absolute value of all elements) will be your answer
+	
+	If number of -ve numbers are odd, then you can cancel out the negative sign 
+	of large numbers so that they contribute to our over all sum.
+	And notice that at the end, you will be left with one negative number.
+	So, shift this negative sign to the smallest number
+*/
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        //we take biggest number in negative(means jiska positive me effect kam ho)
+        int small=INT_MAX;
+        long long count=0;//count negative numbers
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sum += abs(matrix[i][j]);
+                
+                if(matrix[i][j]<0){
+                    count++;
+                }
+                
+                //find smallest number(jiska sum per effect sbse kam ho)
+                small = min(small,abs(matrix[i][j]));
+                
+            }
+        }
+        //simple se baat hai ki agar count positive even hua to sbhe negative number shift       //kr ke khud ko positive kr lenge koi negative nhi bachega to hum directlt sum //return denge
+        if(count%2==0){
+            return sum;
+        }
+        return sum-2*small; 
+/*            agar count odd hua to hum dekhnge ki sum ko maximise krne k liye (count-1) elements to shifting kr k khud ko positive bna lenge bss ek number left reh jayega to maximise krne ke liye hum left us number ko chodenge jiska sum per effect kmm ho. jo ki hum small me nikal chuke */
+/*            2 se multiply isliye q ki ek baar to humne user positive kr k sum me jod rkha tha or ek baar uska negtive effect dalne ke liye
+        */
+    }
+};
