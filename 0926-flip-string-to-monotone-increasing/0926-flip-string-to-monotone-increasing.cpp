5c557087d962e9f19818ed1d7fd414229d0530d2
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-17 12:21:32 +0530

    Time: 38 ms (57.47%), Space: 15.6 MB (36.46%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -22,7 +22,10 @@ public:
         int count_one=0;
         for(int i=1;i<n+1;i++){
            if(s[i-1]=='0'){
-              
+              //then we have 2 option(0,0,1,1,0) let we have on the 4th index then 
+               //2 options-->1. if we dont flip it then before s[i] all elements shoul
+               //be zero means (2 one ko flip krna hoga )
+              //2. if we flip then 1+dp[i-1] ek to flip krne ka and count usser peechey k
                dp[i]=min(count_one,1+dp[i-1]);
                
            }
