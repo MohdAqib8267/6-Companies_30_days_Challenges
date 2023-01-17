0eeca7e6e18d7b42f91acf11fb97138cba411e88
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-17 12:21:21 +0530

    Time: 36 ms (60.25%), Space: 15.5 MB (36.46%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
+        // int n=s.size();
+        // //without DP
+        // int count_zero=0;
+        // int count_flip=0;
+        // for(int i=0;i<n;i++){
+        //     if(s[i]=='0'){
+        //         count_zero++;
+        //     }
+        //     else{
+        //         count_flip++;
+        //     }
+        //     count_zero=min(count_zero,count_flip);
+        // }
+        // return count_zero;
+        
+        //with DP
         int n=s.size();
-        //without DP
-        int count_zero=0;
-        int count_flip=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='0'){
-                count_zero++;
-            }
+        vector<int>dp(n+1,0);
+        int count_one=0;
+        for(int i=1;i<n+1;i++){
+           if(s[i-1]=='0'){
+              
+               dp[i]=min(count_one,1+dp[i-1]);
+               
+           }
             else{
-                count_flip++;
+                count_one++;
+                dp[i]=dp[i-1];
             }
-            count_zero=min(count_zero,count_flip);
         }
-        return count_zero;
+        return dp[n];
     }
 };
