0c436cfab2b2ab5db43e29f899705b39a05c9d6a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-01-25 20:32:33 +0530

    Time: 245 ms (77.55%), Space: 99.1 MB (15.10%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        int i=0;
+         long sum=0;
+         int ans=0;
+         sort(nums.begin(),nums.end());
+       for (int j=0;j<nums.size();j++)
+         {
+           sum+=nums[j];
+            //let's assume, we modify the all previous element till jth element and we would make all previous elements like ith element and check that number of operations less then k or not
+            while(k<(long)(j-i+1)*nums[j] - sum)
+           {
+               sum-=nums[i];
+               i++;
+           }
+            ans=max(ans,j-i+1);
+            
+        }
+        return ans;
+    }
+};
