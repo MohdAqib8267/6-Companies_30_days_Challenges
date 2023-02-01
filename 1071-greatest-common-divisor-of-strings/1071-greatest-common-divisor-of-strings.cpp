afc20d58d85447a0a50b7890f0101ad8c25b0a76
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-01 08:29:15 +0530

    Time: 0 ms (100.00%), Space: 7 MB (88.75%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int find(int a,int b){
+        if(b==0) return a;
+        return find(b,a%b);
+    }
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1){
+            return "";
+        }
+        int gcd = find(str1.size(),str2.size());
+        return str1.substr(0,gcd);
+    }
+};
