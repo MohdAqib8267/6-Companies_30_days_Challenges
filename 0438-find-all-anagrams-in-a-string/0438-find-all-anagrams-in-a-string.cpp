4e47ee3afeb43decfe56fd714018b89ee7154072
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-05 11:47:23 +0530

    Time: 22 ms (49.47%), Space: 8.7 MB (44.04%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,53 +1,34 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        //Giving TLE
-      // int n=s.size();
-      //   int m=p.size();
-      //   string p2=p;
-      //   sort(p2.begin(),p2.end());
-      //   string str=s.substr(0,m);
-      //   sort(str.begin(),str.end());
-      //   vector<int>ans;
-      //   if(str==p2){
-      //       ans.push_back(0);
-      //   }
-      //   for(int i=1;i<=n-m;i++){
-      //       string temp=s.substr(i,m);
-      //       sort(temp.begin(),temp.end());
-      //       if(temp==p2){
-      //           ans.push_back(i);
-      //       }
-      //   }
-      //   return ans;
-        
-        int n=s.size(), m=p.size();
-        unordered_map<char,int>mp;
         vector<int>ans;
-        for(int i=0;i<m;i++){
+        unordered_map<char,int>mp;
+        
+        for(int i=0;i<p.size();i++){
             mp[p[i]]++;
         }
-        int i=0,j=0;
-        int count=mp.size();
-        while(j<n){
-            if(mp.find(s[j])!=mp.end()){
-                mp[s[j]]--;
-                if(mp[s[j]]==0){
-                    count--;
-                }
-                
-            }
-            if(j-i+1<m){
+        int i=0;
+        int j=0;
+        int cnt=mp.size();
+        while(j<s.size()){
+
+           if(mp.find(s[j])!=mp.end()){
+               mp[s[j]]--;
+               if(mp[s[j]]==0){
+                   cnt--;
+               }
+           }
+            if(j-i+1<p.size()){
                 j++;
             }
-            else if(j-i+1 == m){
-                if(count==0){
+            else if(j-i+1==p.size()){
+                if(cnt==0){
                     ans.push_back(i);
                 }
                 if(mp.find(s[i])!=mp.end()){
                     mp[s[i]]++;
                     if(mp[s[i]]==1){
-                        count++;
+                        cnt++;
                     }
                 }
                 i++;
