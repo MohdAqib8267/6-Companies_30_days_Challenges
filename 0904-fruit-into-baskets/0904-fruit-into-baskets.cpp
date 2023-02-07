4d7dd960296054c951e6c645176ad5e5d5395a44
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-07 10:09:47 +0530

    Time: 194 ms (56.09%), Space: 73.2 MB (23.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& nums) {
+    int totalFruit(vector<int>& fruits) {
+        //sliding window maximum at most 2 distinct char
         int i=0;
         int j=0;
-        int ans=INT_MIN;
-        int n=nums.size();
-        int count=0;
+        int n=fruits.size();
+        int ans=0;
+        int cnt=0;
         unordered_map<int,int>mp;
         while(j<n){
-            mp[nums[j]]++;
-            if(mp[nums[j]]==1){
-                count++;
+            mp[fruits[j]]++;
+            if(mp[fruits[j]]==1){
+                cnt++;
             }
-            while(count>2){
-                mp[nums[i]]--;
-                if(mp[nums[i]]==0){
-                    count--;
+            
+            while(cnt>2 and i<=j){
+                mp[fruits[i]]--;
+                if(mp[fruits[i]]==0){
+                    cnt--;
                 }
                 i++;
             }
