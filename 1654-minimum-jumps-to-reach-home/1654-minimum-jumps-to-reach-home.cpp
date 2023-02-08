258993ac0c25c09a98d45f42520cb75e65496a0a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-08 18:55:56 +0530

    Time: 38 ms (80.85%), Space: 18.5 MB (80.71%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+        queue<pair<int, pair<int, bool>>> q; // {node, isBackward}
+        q.push({0, {0, false}});
+
+        set<int> s{forbidden.begin(), forbidden.end()};
+        vector<int> vis(6000);
+        vis[0] = 1;
+
+        int ans = 0;
+        while(!q.empty()) {
+            auto p = q.front();
+            q.pop();
+
+            int steps = p.first, val = p.second.first, back = p.second.second;
+
+            if(val == x) return steps;
+
+            if(!back) {
+                int next_pos = val - b;
+                if(next_pos >= 0 and !vis[next_pos] and !s.count(next_pos)) {
+                    q.push({steps + 1, {next_pos, true}});
+                    vis[next_pos] = 1;
+                }
+            }
+
+            int next_pos = val + a;
+            if(next_pos < 6000 and !vis[next_pos] and !s.count(next_pos)) {
+                q.push({steps + 1, {next_pos, false}});
+                vis[next_pos] = 1;
+            }
+        }
+        
+        return -1;
+    }
+};
