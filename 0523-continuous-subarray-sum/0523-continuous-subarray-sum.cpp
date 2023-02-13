d27c6422ed3146cd5f8b0c797b8fc66e60bb5b7b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-13 18:41:53 +0530

    Time: 240 ms (66.89%), Space: 113.8 MB (77.77%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        unordered_map<int,int>mp;
+          unordered_map<int,int>mp;
         int sum=0;
-        for(int i=0;i<n;i++){
+        int rem=0;
+        for(int i=0;i<nums.size();i++){
             sum=sum+nums[i];
-            int rem=sum%k;
-             if(rem==0 and i>=1){ //bcs ho skta hai 0th index wala hi pura devide ho jaye but hume km se km do element chahye hai
+            rem=sum%k;
+
+            if(rem==0 and i>=1){ //bcs ho skta hai 0th index wala hi pura devide ho jaye but hume km se km do element chahye hai
             return true;
 
             }
-            if(mp.find(rem)!=mp.end()){
+
+            if(mp.find(rem) != mp.end()){
+                //its mean hum remove kr rhey hai jitne element mp[rem] tk ki index per pdey hai
                 if(i-mp[rem]>1){
+                    //check ki ek hi element to nhi bacha subarray me
                     return true;
                 }
             }
             else{
-                mp[rem]=i;
+                 mp[rem]=i; //index first time wali hi rhey
             }
+           
         }
         return false;
     }
