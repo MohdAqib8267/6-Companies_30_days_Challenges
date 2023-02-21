31165bcad29b3de4d7568eba1be3f0489d50cb63
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-21 15:12:55 +0530

    Time: 34 ms (32.30%), Space: 22.4 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,14 +7,14 @@ public:
         }
         int st=0;
         int en=n-1;
-        while(st<en){
+        while(st<=en){
             int mid=(st+en)/2;
             if((mid%2==0 and nums[mid]==nums[mid+1]) or (mid%2 ==1 and nums[mid]==nums[mid-1])){
                 st=mid+1;
             }
          
             else{
-                en=mid;
+                en=mid-1;
             }
         }
         return nums[st];
