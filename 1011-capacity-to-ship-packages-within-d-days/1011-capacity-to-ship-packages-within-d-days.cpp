b8e8474380061bd3ae47a8eab98b15a3a5925d61
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-02-22 16:09:30 +0530

    Time: 86 ms (21.61%), Space: 26.1 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    bool isPossible(vector<int>&weights,int mid,int days){
-        int sum=0;
-        int count=1;
-        for(int i=0;i<weights.size();i++){
-            sum=sum+weights[i];
-            if(sum>mid){
-                count++;
-                sum=weights[i];
-                if(sum>mid || count>days){
-                    return false;
+    //correct
+    // bool isPossible(vector<int>&weights,int mid,int days){
+    //     int sum=0;
+    //     int count=1;
+    //     for(int i=0;i<weights.size();i++){
+    //         sum=sum+weights[i];
+    //         if(sum>mid){
+    //             count++;
+    //             sum=weights[i];
+    //             // if(sum>mid){
+    //             //     return false;
+    //             // }
+    //         }
+    //         if(count>days){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        //optimize search bounds
+        int l=0,r=0,ans;
+        for(int x:weights){
+            l=max(l,x),r+=x;
+        }
+        ans =r;
+        while(l<r){
+            //can I ship all stuff in time if the capcity is "mid"?
+            int mid = (l+r)/2,d=1,cur = 0;
+            for(int i = 0;i<n;i++){
+                if(cur+weights[i]<=mid)cur+=weights[i];
+                else {
+                    d++;
+                    cur = weights[i];
                 }
             }
-            // if(count>days){
-            //     false;
-            // }
-        }
-        return true;
-    }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int n=weights.size();
-        int low=0;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=weights[i];
-        }
-        int high=sum;
-        int ans=sum;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(isPossible(weights,mid,days)){
-                ans=mid;
-                high=mid-1;
-                // if(sum>mid){
-                //     return false;
-                // }
+            if(d<=days){
+                ans = mid;
+                r = mid;
             }
-            else{
-                low=mid+1;
-            }
+            else l = mid+1;
         }
         return ans;
     }
