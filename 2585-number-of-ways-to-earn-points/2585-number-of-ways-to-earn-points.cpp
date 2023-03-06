0f90a2359aa2064701b9d7f7935412ed6cc835ce
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-03-06 12:36:19 +0530

    Time: 719 ms (66.67%), Space: 14.6 MB (33.33%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -37,4 +37,27 @@ public:
         return solve(0,0,target,types,n,dp);
         
     }
+    
+    //2nd method like knapsack
+    //   int dfs(int ind, int target, vector<int>& ques, vector<int>& marks, int n, vector<vector<int>> & dp) {
+    //     int mod = 1e9 + 7;
+    //     if (target == 0) return 1;
+    //     if (target < 0 || ind < 0) return 0;
+    //     if (dp[ind][target] != -1) return dp[ind][target];
+    //     int ans = 0;
+    //     for (int i=0; i<=ques[ind]; i++) {
+    //         ans = (ans + dfs(ind-1, target-marks[ind]*i, ques, marks, n, dp))% mod;
+    //     }
+    //     return dp[ind][target] = ans;
+    // }
+    // int waysToReachTarget(int target, vector<vector<int>>& types) {
+    //     int n = types.size();
+    //     vector<int> ques, marks;
+    //     vector<vector<int>> dp(n, vector<int>(target+1, -1));
+    //     for (auto &type:types) {
+    //         ques.emplace_back(type[0]);
+    //         marks.emplace_back(type[1]);
+    //     }
+    //     return dfs(n-1, target, ques, marks, n, dp);
+    // }
 };
