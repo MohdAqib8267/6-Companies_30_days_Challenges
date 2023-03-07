e1070726d4652d5152f5ba9068ca050a26caa246
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-03-07 13:36:40 +0530

    Time: 546 ms (28.82%), Space: 115.6 MB (69.67%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -11,6 +11,7 @@ public:
             bool ist = 0;
             long long sum=0;
             // first we choose a window of mid size
+            //sliding window
             int i=0,j=0;
             for(j=0;j<mid;j++){
                 while(!q.empty() && ch[q.back()] <= ch[j]) q.pop_back();
@@ -20,10 +21,9 @@ public:
             bdk = (ch[q.front()] + 1ll*(mid)*(sum));
             if(bdk <= bd) ist = 1;
             while(j < ch.size() && (!ist)){
-                // now we remove mid - i th element and then add i'th element
-                // removing element
+               
                 while(!q.empty() && j-q.front()+1 > mid) q.pop_front();
-                // adding element
+                
                 while(!q.empty() && ch[q.back()] <= ch[j]) q.pop_back();
                 q.push_back(j);
                 sum=sum+runc[j]-runc[i];
