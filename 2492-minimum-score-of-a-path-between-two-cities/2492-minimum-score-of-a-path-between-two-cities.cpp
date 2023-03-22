c115e6cf7489b096704d83cee93d81c5222f2f55
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-03-22 17:02:30 +0530

    Time: 605 ms (30.16%), Space: 137.5 MB (37.08%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -13,17 +13,45 @@ public:
         }
     }
     int minScore(int n, vector<vector<int>>& roads) {
-        // graph.resize(n+1);
-        vector<vector<int>>graph[n+1];
-        vector<bool>vis(n+1,false);
-        vis.resize(n+1);
-        // Build Graph
-        for (int i=0;i<roads.size();i++){
-            graph[roads[i][0]].push_back({roads[i][1],roads[i][2]});
-            graph[roads[i][1]].push_back({roads[i][0],roads[i][2]});
-        }
-        dfs(1,graph,vis);
-        return ans;
+        
+//         vector<vector<int>>graph[n+1];
+//         vector<bool>vis(n+1,false);
+        
+//         // Build Graph
+//         for (int i=0;i<roads.size();i++){
+//             graph[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+//             graph[roads[i][1]].push_back({roads[i][0],roads[i][2]});
+//         }
+        //DFS
+        // dfs(1,graph,vis);
+        // return ans;
+        
+        
+        // BFS
+         vector<pair<int,int>>adj[n+1];  //esey initialise much better for 2 elements
+       for(auto it:roads){
+           adj[it[0]].push_back({it[1],it[2]});
+           adj[it[1]].push_back({it[0],it[2]});
+       } 
+       queue<int>q;
+       vector<int>vis(n+1,0);
+       q.push(1);
+       int ans=INT_MAX;
+       while(!q.empty()){
+           int node=q.front();
+           q.pop();
+           vis[node]=1;
+           for(auto it:adj[node]){
+               int nextnode=it.first;
+               int wt=it.second;
+                ans=min(ans,wt);
+               if(vis[nextnode]==0){
+                  
+                   q.push(nextnode);
+               }
+           }
+       }
+       return ans;
     }
     
 };
