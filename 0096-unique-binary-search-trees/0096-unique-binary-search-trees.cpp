5b39db63fd291babf4fcfffa279860eaf4d5b0e1
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-03-28 17:05:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (15.95%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -14,8 +14,23 @@ public:
         }
         return dp[n]=ans;
     }
+    int solveTab(int n){
+        vector<int>dp(n+1,0);
+        dp[0]=dp[1]=1;
+        
+        //i-->no of node
+        for(int i=2;i<=n;i++){
+            //j-->root node
+            for(int j=1;j<=i;j++){
+                dp[i] = dp[i] + dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+    }
     int numTrees(int n) {
-       vector<int>dp(n+1,-1);
-        return solve(n,dp);
+       // vector<int>dp(n+1,-1);
+       //  return solve(n,dp);
+        
+        return solveTab(n);
     }
 };
