3c35667520aa9e5dfe3925c75daa87f683aac065
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-03-28 16:10:12 +0530

    Time: 4 ms (69.83%), Space: 9.7 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -24,8 +24,36 @@ public:
         return dp[index]=min({option1,option2,option3});
         
     }
+    
+    //Tabulation (bottom-up)
+    int solve(vector<int>& days,vector<int>& costs){
+       vector<int>dp(days.size()+1,INT_MAX);
+        dp[days.size()]=0;
+        for(int k=days.size()-1;k>=0;k--){
+              //1 day ticket
+            int option1 = costs[0]+dp[k+1];
+        
+            //7 day ticket
+            int i;
+            for( i=k; i<days.size() && days[i]<days[k]+7;i++ );
+        
+            int option2 = costs[1]+dp[i];
+        
+            //30 day ticket
+            for(i=k; i<days.size() && days[i]<days[k]+30;i++);
+        
+            int option3 = costs[2]+dp[i];
+        
+            dp[k]=min({option1,option2,option3});
+        }
+        
+        return dp[0];
+    }
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         vector<int>dp(days.size()+1,-1);
-        return solve(days,costs,0,dp);
+        // return solve(days,costs,0,dp);
+        //tabu
+        return solve(days,costs);
     }
 };
