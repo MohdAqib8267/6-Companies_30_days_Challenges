1b1b64530c9bc670afbe381852ec86f48253f656
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-03-29 17:18:46 +0530

    Time: 54 ms (27.14%), Space: 21 MB (6.44%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -13,12 +13,30 @@ public:
          
         return dp[i][cnt]=max(take,not_take);
     }
+    int solveTabu(int n,vector<int>&sat){
+        sort(sat.begin(),sat.end());
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        
+        //i-->element
+        for(int i=n-1;i>=0;i--){
+            //j--> no of elements
+            for(int j=n;j>=1;j--){
+                int take = (sat[i]*j)+dp[i+1][j+1];
+                int not_take = dp[i+1][j];
+                dp[i][j]=max(take,not_take);
+            }
+        }
+        return dp[0][1];
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
           int n=satisfaction.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
       
-        sort(satisfaction.begin(),satisfaction.end());
-        return solve(0,satisfaction,1,dp);
+//         sort(satisfaction.begin(),satisfaction.end());
+//         // return solve(0,satisfaction,1,dp);
+        
+        //Tabulation
+        return solveTabu(n,satisfaction);
         
     }
 };
