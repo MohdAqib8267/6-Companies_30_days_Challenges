bd3a7a880a59aed0e1b27cff82fc5207ce039f58
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-04-02 12:54:37 +0530

    Time: 260 ms (51.21%), Space: 99.9 MB (31.79%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        vector<int>pairs;
+        int n=spells.size(), m=potions.size();
+        sort(potions.begin(),potions.end());
+        for(int i=0;i<n;i++){
+            // for(int j=0;i<m;j++){
+            //     if(spells[i]*potions[i]>=success){
+            //         pairs.push_back(m-j);
+            //         j=m;
+            //     }
+            // }//insted of this use binary search bcz we find a index
+            int l=0;
+            int h=m-1;
+            int cnt=0;
+            while(l<=h){
+                long long mid = l+(h-l)/2;
+                if((long long)spells[i]*(long long)potions[mid]>=success){
+                    // cnt=h-mid+1;
+                    cnt=m-mid;
+                    
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            pairs.push_back(cnt);
+        }
+        return pairs;
+    }
+};
