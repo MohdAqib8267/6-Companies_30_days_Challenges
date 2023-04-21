013ac32ec0b88f25767e6080eccdf9236909d0ae
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-04-21 21:47:33 +0530

    Time: 172 ms (59.91%), Space: 12.7 MB (72.64%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -9,15 +9,14 @@ public:
        if(dp[index][minProfit][n]!=-1) return dp[index][minProfit][n];
         int include=0;
         if(n>=group[index]){
-            if(minProfit-profit[index]<=0){
+            if((minProfit-profit[index])<=0){
                 include++;
             }
-            int temp = max(0,minProfit-profit[index]);
-            include= (include + helper(group,profit,n-group[index],temp,index+1) )%mod;
+            int temp=max(0,minProfit-profit[index]);
+            include = include+helper(group,profit,n-group[index],temp,index+1);
         }
-        int exclude = helper(group ,profit,n,minProfit,index+1);
-
-        return dp[index][minProfit][n]= (include  +exclude )%mod;
+       int exclude = helper(group,profit,n,minProfit,index+1);
+       return dp[index][minProfit][n]=(include+exclude)%mod;
    }
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
         memset(dp,-1,sizeof(dp));
