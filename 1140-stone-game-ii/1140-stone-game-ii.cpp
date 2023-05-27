16fab91de9ab154d3d8a958c349441aa771720c4
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-05-27 22:31:15 +0530

    Time: 14 ms (84.86%), Space: 8.1 MB (97.13%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-           int dp[101] [201];
-
-    int helper(int i,int m,vector<int>& piles){
-
-        if(i >= piles.size())
-        return 0;
-
-        if(dp[i][m] !=-1)
-        return dp[i][m];
-
-        int total = 0;
-        int ans = INT_MIN;
-
-        for(int j=0;j<2*m;j++){
-
-            if(i+j < piles.size())
-            total += piles[i+j];
-
-            ans = max(ans,total - helper(i+j+1,max(m,j+1),piles));
+    int dp[101][202];
+    int solve(int i,vector<int>& piles,int M){
+        if(i>=piles.size()){
+            return 0;
         }
-
-        return dp[i][m] = ans;
+        if(dp[i][M]!=-1){
+            return dp[i][M];
+        }
+        int tot=0;
+        int ans=INT_MIN;
+        for(int j=0;j<2*M;j++){
+            if(i+j<piles.size()){
+                tot+=piles[i+j];
+            }
+            ans=max(ans,tot-solve(i+j+1,piles,max(M,j+1)));
+        }
+        return dp[i][M]=ans;
     }
     int stoneGameII(vector<int>& piles) {
-
-        memset(dp,-1,sizeof dp);
-
         int sum=0;
-        for(auto x:piles)
-         sum += x;
-
-
-         int diff = helper(0,1,piles);
-
-         return (sum+diff)/2;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<piles.size();i++){
+            sum+=piles[i];
+        }
+        int diff = solve(0,piles,1);
+        //diff hai (a-b); // a -- alice, b--bob 
+        //if we add ((a+b) + (a-b))/2=a(alice)
+        return (sum+diff)/2;
     }
+    
 };
