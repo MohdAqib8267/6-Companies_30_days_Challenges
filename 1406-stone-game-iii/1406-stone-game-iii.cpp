4f07ee209fc7a66506de8ae77ab3c4417ee4e553
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-05-27 19:50:09 +0530

    Time: 254 ms (71.80%), Space: 132.1 MB (86.26%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -20,11 +20,37 @@ public:
         return dp[i]=max({take1,take2,take3});
     }
     string stoneGameIII(vector<int>& stoneValue) {
+        // memoization
         int n=stoneValue.size();
-        vector<int>dp(n+1,-1);
-        int aliceVal = solve(0,stoneValue,n,dp);
-        cout<<aliceVal;
-        if(aliceVal>0){
+        // vector<int>dp(n+1,-1);
+        // int aliceVal = solve(0,stoneValue,n,dp);
+        // cout<<aliceVal;
+        // if(aliceVal>0){
+        //     return "Alice";
+        // }
+        // else if(aliceVal==0){
+        //     return "Tie";
+        // }
+        // return "Bob";
+        
+        //tabulation
+        vector<int>dp(n+1,0);
+        int i=n-1;
+        while(i>=0){
+             int take1=stoneValue[i]-dp[i+1];
+             int take2=INT_MIN;
+            if(i+1<n){
+                take2=stoneValue[i]+stoneValue[i+1]-dp[i+2];
+            }
+            int take3=INT_MIN;
+            if(i+2<n){
+                take3=stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-dp[i+3]; 
+            }
+            dp[i]=max({take1,take2,take3});
+            i--;
+        }
+        int aliceVal = dp[0];
+         if(aliceVal>0){
             return "Alice";
         }
         else if(aliceVal==0){
