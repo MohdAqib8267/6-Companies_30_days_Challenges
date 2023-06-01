fab733d2f39eb814c10acfc16aed888fc912a895
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-01 19:32:12 +0530

    Time: 14 ms (93.80%), Space: 23.2 MB (86.70%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,31 +1,52 @@
 class Solution {
 public:
-void solve(int i,int j,int m,int n,int &area,vector<vector<int>>& grid){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
-            return;
-        }
-        area++;
-        grid[i][j]=0;
+// void solve(int i,int j,int m,int n,int &area,vector<vector<int>>& grid){
+//         if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
+//             return;
+//         }
+//         area++;
+//         grid[i][j]=0;
         
-        solve(i-1,j,m,n,area,grid);
-        solve(i,j+1,m,n,area,grid);
-        solve(i+1,j,m,n,area,grid);
-        solve(i,j-1,m,n,area,grid);
-        return;
+//         solve(i-1,j,m,n,area,grid);
+//         solve(i,j+1,m,n,area,grid);
+//         solve(i+1,j,m,n,area,grid);
+//         solve(i,j-1,m,n,area,grid);
+//         return;
+//     }
+//     int maxAreaOfIsland(vector<vector<int>>& grid) {
+//         int m=grid.size();
+//         int n=grid[0].size();
+//         int ans=0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(grid[i][j]==1){
+//                     int area=0;
+//                     solve(i,j,m,n,area,grid);
+//                     ans=max(ans,area);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+     int dfs(vector<vector<int>>& grid, int i, int j) {
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) return 0;
+
+        grid[i][j] = 0;
+        return (1 + dfs(grid, i + 1, j)
+        + dfs(grid, i - 1, j)
+        + dfs(grid, i, j + 1)
+        + dfs(grid, i, j - 1));
     }
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int ans=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    int area=0;
-                    solve(i,j,m,n,area,grid);
-                    ans=max(ans,area);
+      int maxAreaOfIsland(vector<vector<int>>& grid) {
+          int maxArea=0;
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
+                if(grid[i][j] == 1) {
+                    maxArea = std::max(maxArea,  dfs(grid, i, j));
                 }
             }
         }
-        return ans;
+
+        return maxArea;
     }
 };
