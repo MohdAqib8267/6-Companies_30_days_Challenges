1dcdb391b6667b03f7dd67339b62d63b3afc7147
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-02 20:53:45 +0530

    Time: 70 ms (32.98%), Space: 18.7 MB (23.51%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.cpp
+++ 1169-invalid-transactions/1169-invalid-transactions.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<string>split(string s){
+      vector<string>split(string s){
          vector<string>str;
         string p="";
         for(int i=0;i<s.size();i++){
@@ -16,38 +16,36 @@ public:
         return str;
     }
     vector<string> invalidTransactions(vector<string>& transactions) {
-        vector<string>ans;
+        int n=transactions.size();
         unordered_map<string,vector<vector<string>>>mp;
-        
-        for(int i=0;i<transactions.size();i++){
-             vector<string>temp =split(transactions[i]);
-            vector<string>rem={temp[1],temp[2],temp[3]};
-                mp[temp[0]].push_back(rem);
-         }
-        
-        
+        vector<string>ans;
+        for(int i=0;i<n;i++){
+            string tr=transactions[i];
+            vector<string>temp=split(tr);
+            //temp=["alice","20","800","mtv"]
+            mp[temp[0]].push_back({temp[1],temp[2],temp[3]});
+        }
         for(auto it:mp){
-            vector<vector<string>>list = it.second;
-            vector<bool>vis(list.size(),false);
-            for(int i=0;i<list.size();i++){
-                if(stoi(list[i][1])>1000){
+            vector<vector<string>>lists=it.second;
+            vector<bool>vis(lists.size(),false);
+            for(int i=0;i<lists.size();i++){
+                if(stoi(lists[i][1])>1000){
                     vis[i]=true;
                 }
-                for(int j=i+1;j<list.size();j++){
-                    if(abs(stoi(list[i][0])-stoi(list[j][0]))<=60 and list[i][2] != list[j][2]){
+                for(int j=i+1;j<lists.size();j++){
+                    if(abs(stoi(lists[i][0])-stoi(lists[j][0]))<=60 and lists[i][2]!=lists[j][2]){
                         vis[i]=true;
                         vis[j]=true;
                     }
                 }
             }
+            
             for(int i=0;i<vis.size();i++){
                 if(vis[i]){
-                    ans.push_back(it.first+","+list[i][0]+","+list[i][1]+","+list[i][2]);
+                    ans.push_back(it.first+","+lists[i][0]+","+lists[i][1]+","+lists[i][2]);
                 }
             }
         }
-        
         return ans;
-    
     }
 };
