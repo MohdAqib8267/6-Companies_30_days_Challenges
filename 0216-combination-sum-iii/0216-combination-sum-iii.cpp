a95f9478d90328ee62c8b343100cf79d0452214b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-04 15:18:40 +0530

    Time: 4 ms (43.46%), Space: 6.5 MB (43.52%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    void solve(int ind,int k,int target,vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans){
-         if(temp.size()==k and target==0){
-            
-                ans.push_back(temp);
-                return;
-            
+    void solve(int i,int target,int k,vector<int>& temp,vector<vector<int>>&ans){
+        if(target==0 and temp.size()==k){
+            ans.push_back(temp);
+            return;
         }
-        if(ind==nums.size()){
-            return ;
+        if(i>9 or target<0){
+            return;
         }
-        //pick
-        temp.push_back(nums[ind]);
-        solve( ind+1, k, target-nums[ind],nums, temp, ans);
-        //not pick
+        temp.push_back(i);
+        solve(i+1,target-i,k,temp,ans);
         temp.pop_back();
-        solve( ind+1, k, target,nums, temp, ans);
+        solve(i+1,target,k,temp,ans);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>nums;
-        //according to 1st condition
-        for(int i=1;i<=9;i++){
-            nums.push_back(i);
-        }
-        
         vector<vector<int>>ans;
         vector<int>temp;
-        solve(0,k,n,nums,temp,ans);
+        int target=n;
+        solve(1,target,k,temp,ans);
         return ans;
-        
     }
 };
