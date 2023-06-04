6f9a3ec6439e6feb7fc8462e4ad99364fa40e7a5
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-05 00:06:43 +0530

    Time: 26 ms (74.83%), Space: 8.2 MB (83.97%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -3,11 +3,10 @@ public:
    int ans=0;
     unordered_map<char,int>mp;
     void solve(int st,vector<string>& arr){
-        int res=0;
+       int res=0;
         for(auto it:mp){
-            if(it.second>1) return;
-            
-            if(it.second==1) res++;
+            if(it.second>1)return;
+            if(it.second==1)res++;
         }
         ans=max(ans,res);
         for(int i=st;i<arr.size();i++){
@@ -15,7 +14,7 @@ public:
                 mp[it]++;
             }
             solve(i+1,arr);
-            for(auto it:arr[i]){
+             for(auto it:arr[i]){
                 mp[it]--;
             }
         }
