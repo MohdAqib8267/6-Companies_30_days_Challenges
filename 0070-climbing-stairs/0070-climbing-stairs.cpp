ad6b4eb7335d7f1c708f9e771325a06e60f6fc80
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-05 13:57:48 +0530

    Time: 4 ms (12.12%), Space: 6.2 MB (36.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,7 +10,15 @@ public:
         return dp[n] = solve(n-1,dp)+solve(n-2,dp);
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(n,dp);
+        
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
