410483dee425984665459ca56015e8693ceec8c3
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-05 18:40:25 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (79.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -17,7 +17,23 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         //memoization
-        vector<int>dp(n+1,-1);
-        return solve(0,n,nums,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(0,n,nums,dp);
+        
+        //Tabulation
+        if(n==1){
+            return nums[0];
+        }
+        vector<int>dp(n+1,0);
+        dp[0]=0;
+        dp[1]=nums[0];
+        dp[2]=max(nums[0],nums[1]);
+        for(int i=3;i<=n;i++){
+            int take = nums[i-1]+dp[i-2];
+            int not_take = dp[i-1];
+            dp[i]=max(take,not_take);
+        }
+        return dp[n];
     }
+   
 };
