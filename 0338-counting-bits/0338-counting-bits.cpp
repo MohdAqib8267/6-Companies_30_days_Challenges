f26869b16a685c5c9d32f94395f699659e410d13
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-05 15:11:08 +0530

    Time: 10 ms (29.77%), Space: 8.5 MB (17.77%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+          vector <int> ans;
+        ans.push_back(0);
+        if(n==0){
+            return ans;
+        }
+        ans.push_back(1);
+        if(n==1){
+             return ans;
+        }
+        else{
+            for(int i=2;i<=n;i++){
+                if(i%2==0){
+                    //even number doesn't brings new 1
+                    ans.push_back(ans[i/2]);
+                }
+                else{
+                    //odd number the units place flips to 1, adding 1;
+                    ans.push_back(ans[i/2]+1);
+                }
+            }
+        }
+        return ans;
+    }
+};
