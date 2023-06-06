4b275e02b68d110141e3dee481d405232151298a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-06 16:11:01 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.50%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,15 @@ public:
         return dp[i][j]=solve(i+1,j,m,n,dp)+solve(i,j+1,m,n,dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(0,0,m,n,dp);
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        // return solve(0,0,m,n,dp);
+        
+        vector<vector<int>>dp(m,vector<int>(n,1));
+           for(int i = 1; i < m; i++) {
+        for(int j = 1; j < n; j++) {
+            dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+    }
+        return dp[m-1][n-1];
     }
 };
