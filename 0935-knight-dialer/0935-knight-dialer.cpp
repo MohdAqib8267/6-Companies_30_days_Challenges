0f486b97742c03ca038d68ce9875100c01327f2c
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-07 19:47:56 +0530

    Time: 183 ms (67.22%), Space: 7.8 MB (73.00%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+       int mod=1e9+7;
+    long dp[5001][5][4];
+    long solve(int n,int row,int col)
+    {
+        if(row<0 || col<0 ||row>=4 || col>=3 || (row==3 && col!=1)) return 0;
+        if(n==1) return 1;// as 10 numbers when n=1
+        if(dp[n][row][col]!=-1) return dp[n][row][col];
+        long take = solve(n-1,row-2,col-1)%mod +
+        solve(n-1,row-2,col+1)%mod + solve(n-1,row-1,col-2)%mod + solve(n-1,row+1,col-2)%mod + 
+        solve(n-1,row+1,col+2)%mod + solve(n-1,row-1,col+2)%mod + solve(n-1,row+2,col-1)%mod + 
+        solve(n-1,row+2,col+1)%mod; //checking all the knight possibilities
+        return dp[n][row][col]=take;
+    }
+    int knightDialer(int n) {
+        memset(dp,-1,sizeof(dp));
+        long s=0;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                s = (s+solve(n,i,j))%mod;//for each and every index of phone dialer as 4*3 grid
+            }
+        }
+        return (int)s;
+    }
+};
