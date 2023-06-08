af7db949a24a3426b4f8fad0367a2c879f18b39d
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-08 14:32:33 +0530

    Time: 4 ms (74.77%), Space: 7.3 MB (68.89%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-         int n = s.length(),maxi=0;
-    stack<int>st;
-    st.push(-1);
-    for(int i=0;i<n;i++)
-    {
-        if(s[i]=='(') st.push(i);
-        else 
-        {
-            st.pop();
-            if(st.empty()) st.push(i);
-            else
-            {
-                int len=i-st.top();
-                maxi=max(maxi,len);
+        int n=s.size();
+        stack<int>st;
+        st.push(-1);
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    int len=i-st.top();
+                    maxi=max(maxi,len);
+                }
             }
         }
-    }
-    return maxi;
+        return maxi;
     }
 };
