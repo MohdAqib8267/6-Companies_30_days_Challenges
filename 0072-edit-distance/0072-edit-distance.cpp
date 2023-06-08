34bb98cd23ec76a42892a2b9b6dd836aed95b90b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-08 19:29:35 +0530

    Time: 7 ms (93.99%), Space: 9.4 MB (19.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int solve(int n,int m,string &word1,string &word2,vector<vector<int>>& dp){
-        if(n==0){
-            return m;
+    int solve(int i,int j,string &s1,string &s2, vector<vector<int>>&dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        if(m==0){
-            return n;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        
-        if(word1[n-1]==word2[m-1]){
-            return dp[n][m]=solve(n-1,m-1,word1,word2,dp);
+        if(s1[i]==s2[j]){
+            return dp[i][j] = solve(i-1,j-1,s1,s2,dp);
         }
         else{
-           int a=solve(n,m-1,word1,word2,dp);//insert
-            int b=solve(n-1,m,word1,word2,dp); //remove;
-            int c=solve(n-1,m-1,word1,word2,dp); //replace
-            return dp[n][m]=1+min({a,b,c});
+            int insert=solve(i,j-1,s1,s2,dp);
+            int dlt=solve(i-1,j,s1,s2,dp);
+            int replace=solve(i-1,j-1,s1,s2,dp);
+            return dp[i][j]=1+min({insert,dlt,replace});
         }
+        
     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,word1,word2,dp);
+        return solve(n-1,m-1,word1,word2,dp);
     }
 };
