3a4023579dc70e666ca074c0c2995a225f57ad95
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-09 12:29:42 +0530

    Time: 18 ms (8.23%), Space: 11.5 MB (68.80%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -11,37 +11,18 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-//         if(head==NULL){
-//             return NULL;
-//         }
-//         ListNode* temp1=head;
-//         ListNode* temp2=head->next;
-        
-//         while(temp2!=NULL){
-//             if(temp1->val == temp2->val){
-//                 temp2=temp2->next;
-//                 temp1->next=temp2;
-//             }
-//             else{
-//                 temp1=temp2;
-//             temp2=temp2->next;
-//             }
-            
-//         }
-//         return head;
-        
-        //recursive
-        if(head==NULL or head->next == NULL){
-            return head;
+        if(head==NULL){
+            return NULL;
         }
-        if(head->val == head->next->val){
-            ListNode* temp = head->next;
-            head->next = head->next->next;
-            delete(temp);
-            deleteDuplicates(head);
-            
+        ListNode* temp=head;
+        while(temp->next!=NULL){
+            if(temp->val == temp->next->val){
+                temp->next=temp->next->next;
+                
+            }
+            else
+            temp = temp->next;
         }
-        deleteDuplicates(head->next);
         return head;
     }
 };
