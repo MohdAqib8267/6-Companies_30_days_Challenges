0f81f0c55101e81b31437b818917af4e575c6198
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-14 12:49:36 +0530

    Time: 15 ms (42.79%), Space: 20.7 MB (5.56%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -21,10 +21,26 @@ public:
         ans=max(ans,1+lh+rh);
         return 1+max(lh,rh);
     }
+    
+    int calcDiameter(TreeNode* root,int *height){
+        if(root==NULL) return 0;
+        
+        int lh=0, rh=0;
+        int leftDia = calcDiameter(root->left,&lh);
+        int rightDia = calcDiameter(root->right,& rh);
+        *height = 1+max(lh,rh);
+        int currDia = 1+lh+rh;
+        return max({currDia,leftDia,rightDia});
+    }
     int diameterOfBinaryTree(TreeNode* root) {
-        int ans=INT_MIN;
-        solve(root,ans);
-        return ans-1;
+        //Methid 1
+        // int ans=INT_MIN;
+        // solve(root,ans);
+        // return ans-1;
+        
+        //Method 2
+        int height=0;
+        return calcDiameter(root,&height)-1;
 
     }
 };
