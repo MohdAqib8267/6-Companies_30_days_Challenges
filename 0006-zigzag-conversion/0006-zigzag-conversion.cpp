a7c1182d1ceb2919d8cccbbd9d98f977664fe2ff
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-19 19:56:21 +0530

    Time: 30 ms (26.91%), Space: 16.7 MB (14.83%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<vector<char>>mat(numRows);
-        int g=0;
-        int i=0;
-        int flag=1;
         int n=s.size();
-        string ans="";
+        int g=0;
+        vector<vector<char>>mat(numRows);
+        int i=0;
+        bool flag=1;
         if(n==1 || numRows==1)
         return s;
         while(i<n){
-            if(flag==1){
+            if(flag){
                 while(i<n and g<numRows){
                     mat[g++].push_back(s[i]);
                     i++;
                 }
                 flag=0;
+                i--;
             }
             else{
                 int k=numRows-2;
-                while(k>=0 and i<n){
+                while(i<n and k>=0){
                     mat[k--].push_back(s[i]);
                     i++;
                 }
                 flag=1;
                 g=1;
+                i--;
             }
+            i++;
         }
+       string ans;
         for(auto it:mat){
-            for(auto jt:it){
-                ans+=jt;
+            for(auto gt:it){
+                ans.push_back(gt);
             }
         }
         return ans;
