2ccf190bc378e175d3f82b22c37997bef75df885
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-22 14:17:08 +0530

    Time: 12 ms (67.38%), Space: 7 MB (77.97%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        //sliding window
+        int n=s.size();
+        unordered_map<char,int>mp;
+        int l=0;
+        int r=0;
+        int res=0;
+        int maxCount=0;
+        while(r<n){
+            mp[s[r]]++;
+            //now check maximum freq of char(insted of scan whole map using maxCount variable that keep count maximum frequency character)
+            maxCount=max(maxCount,mp[s[r]]); //give maxCount element in current window
+            //now find current window length and check condition
+            int currWindowLen = r-l+1;
+            if(currWindowLen-maxCount <= k){
+                res=max(res,currWindowLen);
+            }
+            else{
+                //substring condition ko meet nhi krti to shrink the current window
+                mp[s[l]]--;
+                l++;
+                // continue;
+            }
+            r++;
+        }
+        return res;
+    }
+};
