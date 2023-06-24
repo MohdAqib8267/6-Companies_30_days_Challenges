fc99acfd7401229eef43d47db774f9410f54053b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-24 14:59:11 +0530

    Time: 8 ms (95.03%), Space: 23.9 MB (66.52%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -13,29 +13,30 @@ class Solution {
 public:
     unordered_map<TreeNode*,int>mp;
     int solve(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
-          if(mp.find(root)!=mp.end()){
-            return mp[root];
-        }
+        if(root==NULL) return 0;
         if(root->left==NULL and root->right==NULL){
             return root->val;
         }
-        //if we take the current root then we can take in next step, grandchild child of this root
-        int take = root->val;
-       if(root->left){  
-        take+=solve(root->left->left)+solve(root->left->right);
+        if(mp.find(root)!=mp.end()){
+            return mp[root];
         }
+        int take=root->val;
+        if(root->left){
+           take +=  solve(root->left->left)
+                    +solve(root->left->right);
+            }
         if(root->right){
-        take+=solve(root->right->left)+solve(root->right->right);
+            take += solve(root->right->left)
+                    +solve(root->right->right);
         }
+                    
         
-        //if we dont take current root then we can take child root
         int not_take = solve(root->left)+solve(root->right);
-        return mp[root]=max(take,not_take);
+        return mp[root] = max(take,not_take);
+        
+        
     }
     int rob(TreeNode* root) {
-        return solve(root);
+       return solve(root);
     }
 };
