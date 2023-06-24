ccdc99755d2e36c136cec4f3d62ee8dfa05c90d3
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-24 15:46:43 +0530

    Time: 63 ms (32.66%), Space: 15.9 MB (65.24%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-   int dfs(int i, int j, int n, int m, vector<vector<int>>& mat, int val, vector<vector<int>> &dp)
-    {
-        if(i<0 || j<0 || i>=n || j>=m || mat[i][j]<=val)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int a = 1+dfs(i+1, j, n, m, mat, mat[i][j], dp);
-        int b = 1+dfs(i, j+1, n, m, mat, mat[i][j], dp);
-        int c = 1+dfs(i-1, j, n, m, mat, mat[i][j], dp);
-        int d = 1+dfs(i, j-1, n, m, mat, mat[i][j], dp);
-        return dp[i][j]=  max({a,b,c,d});
+    int solve(int i,int j,int m,int n,vector<vector<int>>& matrix,vector<vector<int>>& dp){
+        int up=0,left=0,down=0,right=0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i-1>=0 and matrix[i-1][j]>matrix[i][j]){
+            up=1+solve(i-1,j,m,n,matrix,dp);
+        }
+        if(j-1>=0 and matrix[i][j-1]>matrix[i][j]){
+            left=1+solve(i,j-1,m,n,matrix,dp);
+        }
+        if(i+1<m and matrix[i+1][j]>matrix[i][j]){
+            down=1+solve(i+1,j,m,n,matrix,dp);
+        }
+        if(j+1<n and matrix[i][j+1]>matrix[i][j]){
+            right=1+solve(i,j+1,m,n,matrix,dp);
+        }
+        return dp[i][j]=max({up,left,down,right});
     }
-    
-    int longestIncreasingPath(vector<vector<int>>& mat) {
-        
-        int n = mat.size();
-        int m = mat[0].size();
-        int ans=1;
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                ans=max(ans, dfs(i,j,n,m,mat, INT_MIN, dp));
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int ans=0;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans=max(ans,solve(i,j,m,n,matrix,dp));
             }
         }
-        return ans;
+        return 1+ans;
     }
 };
