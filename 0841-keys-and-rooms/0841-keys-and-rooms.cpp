0c7b6e30127d8c8811c747b78c7bb034fe895697
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-28 10:49:31 +0530

    Time: 13 ms (28.75%), Space: 11 MB (14.25%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void DFS(int src,vector<bool>& vis,vector<int>adj[]){
+    void dfs(int src,vector<bool>& vis,vector<int>adj[]){
         vis[src]=true;
         for(auto it:adj[src]){
             if(!vis[it]){
-                DFS(it,vis,adj);
+                dfs(it,vis,adj);
             }
         }
-        return;
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
         vector<int>adj[n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<rooms[i].size();j++)
+            for(int j=0;j<rooms[i].size();j++){
                 adj[i].push_back(rooms[i][j]);
+            }
         }
         vector<bool>vis(n,false);
-        DFS(0,vis,adj);
+        dfs(0,vis,adj);
         for(int i=0;i<n;i++){
-            if(vis[i]!=true){
+            if(!vis[i]){
                 return false;
             }
         }
