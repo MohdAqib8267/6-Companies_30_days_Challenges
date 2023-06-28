18dfcdb2c62207a2e9ab48b549de751b892f9043
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-28 12:31:26 +0530

    Time: 163 ms (30.90%), Space: 50.5 MB (37.98%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     vector<int>parent,sz;
     int find_parent(int v){
         if(parent[v]==v){
@@ -19,39 +20,73 @@ public:
         parent[b]=a;
         sz[a]+=sz[b];
     }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        parent.resize(n);
-        sz.resize(n,1);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
+    
+    //2nd method
+      void DFS(int src,vector<int>adj[],vector<int>& vis){
+        vis[src]=true;
+        for(auto it:adj[src]){
+            if(!vis[it]){
+                DFS(it,adj,vis);
+            }
         }
-        int m=connections.size();
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        //1st method is DSU
+        
+        // parent.resize(n);
+        // sz.resize(n,1);
+        // for(int i=0;i<n;i++){
+        //     parent[i]=i;
+        // }
+        // int m=connections.size();
+        // if(n-1>m){
+        //     return -1;
+        // }
+        // int cnt=0;
+        // for(int i=0;i<connections.size();i++){
+        //     int u=connections[i][0];
+        //     int v=connections[i][1];
+        //     int x=find_parent(u);
+        //     int y=find_parent(v);
+        //     if(x==y){
+        //         cnt++;
+        //     }
+        //     else{
+        //         make_union(u,v);
+        //     }
+        // }
+        // int comp=0;
+        // for(int i=0;i<n;i++){
+        //     if(parent[i]==i){
+        //         comp++;
+        //     }
+        // }
+        // //like 3 comp and 2 node hai to connect kr payenge
+        // if(comp-1 > cnt){
+        //     return -1;
+        // }
+        // return comp-1;
+        
+        //2nd method-> we can directly count components and our ans would be no of components-1
+        int m=connections.size();//nodes
+        //connectd hone k liye m-1 branches  to honiii hi chahiye
         if(n-1>m){
             return -1;
         }
+        vector<int>adj[n];
+        for(int i=0;i<m;i++){
+            adj[connections[i][0]].push_back(connections[i][1]);
+            adj[connections[i][1]].push_back(connections[i][0]);
+        }
+        vector<int>vis(n,false);
         int cnt=0;
-        for(int i=0;i<connections.size();i++){
-            int u=connections[i][0];
-            int v=connections[i][1];
-            int x=find_parent(u);
-            int y=find_parent(v);
-            if(x==y){
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                DFS(i,adj,vis);
                 cnt++;
             }
-            else{
-                make_union(u,v);
-            }
         }
-        int comp=0;
-        for(int i=0;i<n;i++){
-            if(parent[i]==i){
-                comp++;
-            }
-        }
-        //like 3 comp and 2 node hai to connect kr payenge
-        if(comp-1 > cnt){
-            return -1;
-        }
-        return comp-1;
+        return cnt-1;
     }
+  
 };
