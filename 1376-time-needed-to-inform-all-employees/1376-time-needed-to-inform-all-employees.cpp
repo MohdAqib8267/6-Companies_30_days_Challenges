0de052b3f44d90fc6eee0938c1c975e78c6ad600
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-28 16:15:21 +0530

    Time: 360 ms (36.27%), Space: 119.1 MB (55.87%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -9,11 +9,54 @@ public:
         return ans;
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-       vector<int>adj[n];
-        for(int i=0;i<n;i++){
-            if(manager[i]!=-1)
-            adj[manager[i]].push_back(i);
+       // vector<int>adj[n];
+       //  for(int i=0;i<n;i++){
+       //      if(manager[i]!=-1)
+       //      adj[manager[i]].push_back(i);
+       //  }
+       //  return dfs(headID,adj,manager,informTime,n);
+        
+        
+        //2 BFS
+        
+        int src = headID;
+        // make a adj list 
+
+        vector<int> adj[n];
+        for(int i = 0;i<n;i++){
+            int temp = manager[i];
+            if(temp != -1){
+                adj[temp].push_back(i);
+            }
         }
-        return dfs(headID,adj,manager,informTime,n);
+        vector<int> vis(n,0);
+        queue<int> q;
+        q.push(src);
+        vis[src] = 1;
+        int ans = 0;
+        vector<int> tt(n,0);
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i = 0;i<sz;i++){
+                int temp = q.front();
+                q.pop();
+                
+                for(auto it:adj[temp]){
+                    if(vis[it] == 0){
+                        q.push(it);
+                        tt[it] = tt[temp] + informTime[temp];
+                        ans = max(ans,tt[it]);
+                        vis[it] = 1;
+
+                    }
+                }                
+            }
+
+        }
+       
+        
+        return ans;
+
     }
+    
 };
