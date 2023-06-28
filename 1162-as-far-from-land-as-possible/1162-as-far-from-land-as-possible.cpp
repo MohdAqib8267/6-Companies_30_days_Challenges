7bbac6b18acaac1692f9ca989f7926b9ecbadef4
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-28 13:06:47 +0530

    Time: 76 ms (61.82%), Space: 19.8 MB (84.99%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        //BFS
-        
+        //DFS
         int n=grid.size();
         queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-             if(grid[i][j]==1){
-                 q.push({i,j});
-             }
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    q.push({i,j});
+                }
             }
         }
         if(q.size()==(n*n)){
             return -1;
         }
         int ans=0;
-        vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
+         vector<pair<int,int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.empty()){
-            int size=q.size();
-            for(int ind=0;ind<size;ind++){
-                int i=q.front().first;
-                int j=q.front().second;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                auto p=q.front();
+                int r=p.first;
+                int c=p.second;
                 q.pop();
-                for(int k=0;k<4;k++){
-                    int i1=i+dir[k].first;
-                    int j1=j+dir[k].second;
-                    if(i1>=0 && i1<n && j1>=0 && j1<n && grid[i1][j1]==0){
-                        grid[i1][j1]=1;
-                        q.push({i1,j1});
+                for(int j=0;j<4;j++){
+                    int r1=r+dir[j].first;
+                    int c1=c+dir[j].second;
+                    if(r1>=0 and c1>=0 and r1<n and c1<n and grid[r1][c1]==0){
+                        q.push({r1,c1});
+                        grid[r1][c1]=1;
                     }
                 }
-                
             }
-           ans++;
+            ans++;
         }
         return ans-1;
     }
