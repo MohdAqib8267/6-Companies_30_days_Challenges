ac3d24357b56bf2fdfdf6a9b63f25bdee643b195
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-28 13:57:05 +0530

    Time: 350 ms (19.03%), Space: 14.1 MB (89.48%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    void DFS(int ind,vector<bool>& vis,int n,vector<vector<int>>& stones){
-        vis[ind]=true;
+    void DFS(int src,vector<vector<int>>& stones,vector<int>& vis,int n){
+        vis[src]=1;
         for(int i=0;i<n;i++){
-            if(!vis[i] and (stones[ind][0]==stones[i][0] || stones[ind][1]==stones[i][1])){
-                DFS(i,vis,n,stones);
+            if(!vis[i] and (stones[src][0]==stones[i][0] || stones[src][1]==stones[i][1])){
+                DFS(i,stones,vis,n);
             }
         }
-        return;
     }
     int removeStones(vector<vector<int>>& stones) {
-        //basically this is connected components based question
         int n=stones.size();
-        vector<bool>vis(n,false);
+        //find, connected components
+        //so given conditions se connected wale component find krenege
+        //ab remove kr payenge (n-cnt)
+        vector<int>vis(n,0);
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(vis[i]){
-                continue;
+            if(!vis[i]){
+                DFS(i,stones,vis,n);
+                cnt++;
             }
-            DFS(i,vis,n,stones);
-            cnt++; //gives no of connected components
         }
-        //jo connected honge let m unme m-1 nikal jayege ek reh jayega
-        //so isliye (n-count)
-        return n-cnt;
+        // to jo ek connected component hoga wo apne neighbor ko sbko remove kar payega
+        //sirf ek element backeyga component me baki sab remove me count honge
+        
+        // to short me her component me ek element backeyga
+        // to total elements me se bss us 1 bachey hue ko minus kar do, remove wale aa jayenge
+        return n-cnt; // man lo cnt 1 hi bacha
     }
 };
