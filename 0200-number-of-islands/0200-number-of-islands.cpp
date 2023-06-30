f4be505c612259f8b4389dfd5442c12b22a6147b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-06-30 15:55:33 +0530

    Time: 38 ms (81.33%), Space: 18.1 MB (30.60%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    void DFS(int i,int j,int m,int n,vector<vector<char>>&grid){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0' || grid[i][j]=='2'){
-            return;
-        }
-        grid[i][j]='2';
-        DFS(i,j-1,m,n,grid);
-        DFS(i-1,j,m,n,grid);
-        DFS(i,j+1,m,n,grid);
-        DFS(i+1,j,m,n,grid);
-        
+    void bfs(int i,int j,vector<vector<char>>&grid,int m,int n){
+        grid[i][j]='0';
+         queue<pair<int,int>>q;
+       int dirx[4]={-1,0,1,0};
+        int diry[4]={0,-1,0,1};
+             q.push({i,j});
+            while(!q.empty()){
+                        int sz=q.size();
+                        for(int z=0;z<sz;z++){
+                            auto p=q.front(); q.pop();
+                            int r=p.first, c=p.second;
+                            
+                            for(int k=0;k<4;k++){
+                                int nr=r+dirx[k];
+                                int nc=c+diry[k];
+                                if(nr<m and nc<n and nr>=0 and nc>=0 and grid[nr][nc]=='1'){
+                                    grid[nr][nc]='0';
+                                    q.push({nr,nc});
+                                }
+                            }
+                        }
+                        
+                    }
     }
     int numIslands(vector<vector<char>>& grid) {
+        //using BFS
+        
         int m=grid.size();
         int n=grid[0].size();
-        int count=0;
+     
+        int island=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    DFS(i,j,m,n,grid);
-                    count++;
+                   
+                   island++;
+                    bfs(i,j,grid,m,n);
                 }
             }
         }
-        return count;
+        return island;
     }
 };
