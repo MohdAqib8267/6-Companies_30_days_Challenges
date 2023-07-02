f9236cf52a3e11f52c6c1c440735ce3b09789819
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-02 16:33:09 +0530

    Time: 18 ms (78.65%), Space: 13.1 MB (60.30%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
-    int n = board.size();
-    vector<int> flattenedBoard((n * n) +1);
-    int index = 1;
-    bool leftToRight = true;
-    for (int i = n - 1; i >= 0; i--) {
-        if (leftToRight) {
-            for (int j = 0; j < n; j++) {
-                flattenedBoard[index++] = board[i][j];
+        int n=board.size();
+        vector<int>flattenBoard((n*n)+1);
+        int ind=1;
+        bool leftToright=true;
+        for(int i=n-1;i>=0;i--){
+            if(leftToright){
+                for(int j=0;j<n;j++){
+                    flattenBoard[ind++]=board[i][j];
+                } 
             }
-        } else {
-            for (int j = n - 1; j >= 0; j--) {
-                flattenedBoard[index++] = board[i][j];
+            else{
+                for(int j=n-1;j>=0;j--){
+                    flattenBoard[ind++]=board[i][j];
+                }
+            } 
+            leftToright = !leftToright;
+        }
+        queue<int>q;
+        vector<int>dist((n*n)+1,-1);
+        q.push(1);
+        dist[1]=0;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            if(curr==n*n){
+                return dist[curr];
+            }
+            for(int i=1;i<=6;i++){
+                int next=curr+i;
+                if(next>n*n){
+                   continue; 
+                } 
+                if(flattenBoard[next]!=-1){
+                    next=flattenBoard[next];
+                }
+                if(dist[next]==-1){
+                    dist[next]=dist[curr]+1;
+                    q.push(next);
+                }
             }
         }
-        leftToRight = !leftToRight;
-    }
-    vector<int> dist((n * n)+1, -1);
-    queue<int> q;
-    q.push(1);
-    dist[1] = 0;
-    while (!q.empty()) {
-        int curr = q.front();
-        q.pop();
-        if (curr == n * n ) {
-            return dist[curr];
-        }
-        for (int i = 1; i <= 6; i++) {
-            int next = curr + i;
-            if (next > n * n) {
-                continue;
-            }
-            if (flattenedBoard[next] != -1) {
-                next = flattenedBoard[next] ;
-            }
-            if (dist[next] == -1) {
-                dist[next] = dist[curr] + 1;
-                q.push(next);
-            }
-        }
-    }
-    return -1;
+        return -1;
     }
 };
