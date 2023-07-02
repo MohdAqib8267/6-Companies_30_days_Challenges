1a1905c06ec60b3f6fe67ba0eab6b3f38b7e3486
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-02 19:21:54 +0530

    Time: 10 ms (25.40%), Space: 8.9 MB (71.96%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-       int i=0;
-        int ansIndex =0;
-        while(i<chars.size())
-        {
+        int i=0;
+        int n=chars.size();
+        int ind=0;
+        while(i<n){
             int j=i+1;
-            while(j<chars.size() && chars[i]==chars[j])
-            {
+            while(j<n and chars[j]==chars[i]){
                 j++;
             }
-           // hum yaaha per ansIndex++ islye kar re kyu ki har value k sath yeah apane aap badheeeee
-            chars[ansIndex++]=chars[i];
-
-            int count = j-i;
-
-            if(count>1)
-            { 
-                
-                
-                 //yeah islye kya kyu ki hame 1 2 alag hona agar 12 answer aaay toh 
-
-
-                string cnt= to_string(count);
-                for(char ch: cnt)
-                {
-                    chars[ansIndex++]=ch;
+            chars[ind++]=chars[i];
+            int cnt=j-i;
+            if(cnt>1){
+                string len=to_string(cnt);
+                for(auto ch:len){
+                    chars[ind++]=ch;
                 }
             }
-
-            //agar hum soche toh jo j ki location hogi vhoi h next char ki location hogee 
             i=j;
         }
-             //hume bola gaay tha ki humee size return karni hai aur ...int function diya hua hai
-        return ansIndex;
+        return ind;
     }
 };
