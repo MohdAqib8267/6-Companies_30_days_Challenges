723a3dc48a1d5e3c4cf565406cb9212c7ab17d3f
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-02 18:28:35 +0530

    Time: 18 ms (19.04%), Space: 5.9 MB (58.07%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int solve(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return solve(n-1)+solve(n-2);
+    }
+    int fib(int n) {
+        return solve(n);
+    }
+};
