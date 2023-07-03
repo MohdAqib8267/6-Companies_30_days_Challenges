c3230b322bfbda900cfe7a7ae8792135f0828fb8
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-03 15:35:30 +0530

    Time: 266 ms (5.73%), Space: 45.3 MB (99.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>>pq(nums.begin(),nums.end());
-        while(!pq.empty() and k!=1){
-            pq.pop();
-            k--;
+    int partition(vector<int>& nums,int l,int r){
+        int i=l-1;
+        int pivot=nums[r]; //pivot ke left me pivot se chote abd right me bdey ho
+        for(int j=l;j<=r-1;j++){
+            if(nums[j]<=pivot){
+                i++;
+                swap(nums[i],nums[j]);
+            }
         }
-        return pq.top();
-        
+        swap(nums[i+1],nums[r]);
+        return i+1;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        int lo=0;
+        int hi=n-1;
+        k=n-k; //kth smallest element
+        while(lo<hi){
+            int pi=partition(nums,lo,hi);
+            if(pi<k){
+                lo=pi+1;
+            }
+            else if(pi>k){
+                hi=pi-1;
+            }
+            else{
+                break;
+            }
+        }
+        return nums[k];
     }
 };
