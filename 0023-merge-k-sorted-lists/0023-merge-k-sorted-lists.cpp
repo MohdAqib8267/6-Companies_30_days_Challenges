7d2effc55a05c2874615b7ef09e0689394ba06b1
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-04 16:10:59 +0530

    Time: 16 ms (98.67%), Space: 13.2 MB (67.59%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,51 +10,35 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* head1,ListNode* head2){
-        if(head1==NULL) return head2;
-        if(head2==NULL) return head1;
-        if(head1==NULL and head2==NULL) return NULL;
-        ListNode* dummy=new ListNode(-1);
-        ListNode* temp=dummy;
-        ListNode* temp1=head1;
-        ListNode* temp2=head2;
-        while(temp1!=NULL and temp2!=NULL){
-            if(temp1->val<=temp2->val){
-                temp->next=temp1;
-                temp=temp->next;
-                temp1=temp1->next;
-            }
-            else{
-                temp->next=temp2;
-                temp=temp->next;
-                temp2=temp2->next;
-            }
+    struct compare {
+        bool operator()(ListNode* l1, ListNode* l2) {
+            return l1->val > l2->val;
         }
-        while(temp1!=NULL){
-             temp->next=temp1;
-                temp=temp->next;
-                temp1=temp1->next;
-        }
-        while(temp2!=NULL){
-             temp->next=temp2;
-                temp=temp->next;
-                temp2=temp2->next;
-        }
-        return dummy->next;
-    }
+    };
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n=lists.size();
-        if(n==0){
-            return NULL;
-        }
-        if(n==1){
-            return lists[0];
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        
+        // Initialize priority queue
+        for(ListNode* list: lists) {
+            if(list) {
+                pq.push(list);
+            }
         }
         
-        int j=1;
-        while(j<n){
-            lists[0]=merge(lists[0],lists[j++]);
+        ListNode* dummy = new ListNode(-1); // dummy node to get the head of merged lists
+        ListNode* curr = dummy;
+        
+        while(!pq.empty()) {
+            ListNode* temp = pq.top();
+            pq.pop();
+            curr->next = temp;
+            curr = curr->next;
+            if(temp->next) {
+                pq.push(temp->next);
+            }
         }
-        return lists[0];
+        
+        return dummy->next;
     }
 };
