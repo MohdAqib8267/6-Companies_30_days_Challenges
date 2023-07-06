e2a00bb732120e98fcf9e5a32c75c89f9012823a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-07 02:31:54 +0530

    Time: 83 ms (43.27%), Space: 28 MB (14.70%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -30,10 +30,54 @@ public:
         }
         
     }
+    bool solveTab(string &s, string &p){
+         int n=s.size();
+        int m=p.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        //1st base case
+        dp[0][0]=true;
+        //2nd base case
+        //all rows and 1st col
+        //set false--> it is already false, bcz dp initialize with 0
+        
+        //3rd base case
+        //1st row and all columns
+        for(int j=1;j<=p.size();j++){
+            bool flag=true;
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+            dp[0][j]=flag;
+        }
+        
+        //match
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                     dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                  dp[i][j]= false;
+                }
+            }
+        }
+        return dp[n][m];
+    }
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(s,p,n,m,dp);
+        //solve memoization (n-->0 && m-->0)
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return solve(s,p,n,m,dp);
+        
+        //solve using tabulation(0-->n && 0-->m)
+        return solveTab(s, p);
     }
 };
