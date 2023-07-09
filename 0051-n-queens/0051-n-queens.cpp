bfd912460eba1af20f59c1753e36c6c85acb44f9
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-09 18:46:37 +0530

    Time: 3 ms (92.45%), Space: 7.8 MB (38.32%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,69 +1,80 @@
 class Solution {
 public:
-   void addSol(vector<vector<string>>&ans,vector<vector<int>>&board,int n){
-    vector<string> temp;
-    for(int i=0;i<n;i++){
-    string str="";
-        for(int j=0;j<n;j++){
-            if(board[i][j]==1){
-                str.push_back('Q');
-            }
-            else{
-                str.push_back('.');
-            }
-        }
-        temp.push_back(str);
-    }
-    ans.push_back(temp);
-}
-    bool isSafe(int row,int col,vector<vector<int>>& board,int n){
-        //check in row(we add queeen ----> in left to right direction)
-        int x=row; int y=col;
+    bool isSafe(int i,int j,vector<vector<int>>&grid,int n){
+        int x=i;
+        int y=j;
+        //row left
         while(y>=0){
-            if(board[x][y]==1){
+            if(grid[x][y]==1){
                 return false;
             }
             y--;
         }
-        // we check only in left up and left down daigonal bcz we add queen in board left to right
-         x=row; y=col;
+        //row right--> (not required)
+        
+        //up-left diagonal
+        x=i, y=j;
         while(x>=0 and y>=0){
-            if(board[x][y]==1){
+            if(grid[x][y]==1){
                 return false;
             }
             x--;
             y--;
         }
-        
-          x=row;  y=col;
+        //down left diagonal
+        x=i, y=j;
         while(x<n and y>=0){
-            if(board[x][y]==1){
+            if(grid[x][y]==1){
                 return false;
             }
             x++;
             y--;
         }
+        //col down
+        x=i,y=j;
+        while(x<n){
+            if(grid[x][y]==1){
+                return false;
+            }
+            x++;
+        }
         return true;
+        
     }
-    void solve(int col, vector<vector<int>>& board,vector<vector<string>>& ans,int n){
+    void addans(vector<vector<int>>&grid,vector<vector<string>>&ans,int n){
+       vector<string>temp;
+        for(int i=0;i<n;i++){
+             string res;
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    res.push_back('Q');
+                }
+                else{
+                     res.push_back('.');
+                }
+               
+            }
+            temp.push_back(res);
+        }
+        ans.push_back(temp);
+    }
+    void solve(int col,vector<vector<int>>&grid,vector<vector<string>>&ans,int n){
         if(col==n){
-            addSol(ans,board,n);
+            addans(grid,ans,n);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
-                //put queen in box
-                board[row][col]=1;
-                solve(col+1,board,ans,n);
-                //backtrack
-                board[row][col]=0;
+            if(isSafe(row,col,grid,n)){
+                grid[row][col]=1;
+                solve(col+1,grid,ans,n);
+                grid[row][col]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<int>>board(n,vector<int>(n,0));
+        vector<vector<int>>grid(n,vector<int>(n,0));
         vector<vector<string>>ans;
-        solve(0,board,ans,n);
+        solve(0,grid,ans,n);
         return ans;
     }
 };
