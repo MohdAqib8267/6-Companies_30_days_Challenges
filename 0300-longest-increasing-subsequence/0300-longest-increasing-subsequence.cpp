353aea0846206311acd3937c112c7b3ea9522945
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-09 20:03:15 +0530

    Time: 1631 ms (12.08%), Space: 292.3 MB (8.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
-    int solve(int i,int prev,vector<int>& nums,int n,vector<vector<int>>&dp){
-        if(i>=n){
+    int solve(int i,int prev,vector<int>& nums,vector<vector<int>>&dp){
+        if(i>=nums.size()){
             return 0;
         }
         if(dp[i][prev+1]!=-1){
             return dp[i][prev+1];
         }
-        int take=INT_MIN;
+        int take=0,not_take=0;
         if(prev==-1 || nums[prev]<nums[i]){
-            take=1+solve(i+1,i,nums,n,dp);
+            take=1+solve(i+1,i,nums,dp);
         }
-        int not_take=solve(i+1,prev,nums,n,dp);
-        return dp[i][prev+1] = max(take,not_take);
+        not_take=solve(i+1,prev,nums,dp);
+        return dp[i][prev+1]=max(take,not_take);
+        
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-       // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-       //  return solve(0,-1,nums,n,dp);
-        
-        //Tabulation (simple)
-        vector<int>dp(n+1,1);
-        int ans=1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],1+dp[j]);
-                }
-                ans=max(ans,dp[i]);
-            }
-        }
-        return ans;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        return solve(0,-1,nums,dp);
     }
 };
