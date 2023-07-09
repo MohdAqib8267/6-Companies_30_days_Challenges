0a9b9857c99aeeca837c5b1392e0b78ad0012c67
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-09 23:46:03 +0530

    Time: 563 ms (39.58%), Space: 10.4 MB (67.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,10 +13,24 @@ public:
         }
         not_take=solve(i+1,prev,nums,dp);
         return dp[i][prev+1]=max(take,not_take);
-        
+    }
+    int solveTab(vector<int>& nums){
+        int n=nums.size();
+        vector<int>dp(n+1,1);
+        int ans=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+                ans=max(ans,dp[i]);
+            }
+        }
+        return ans;
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return solve(0,-1,nums,dp);
+        // vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        // return solve(0,-1,nums,dp);
+        return solveTab(nums);
     }
 };
