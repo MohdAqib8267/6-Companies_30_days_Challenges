ea22381a6d9b1c00af656d3791a48710c9e69cc2
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-10 18:52:04 +0530

    Time: 38 ms (84.45%), Space: 18.8 MB (93.18%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-//     static bool cmp(vector<int>& a,vector<int>& b){
-        
-//     }
+
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>ans;
-        sort(intervals.begin(),intervals.end());
-        int i=0;
-        int j=1;
-        while(j<intervals.size()){
-            if(intervals[i][1]>=intervals[j][0]){
-                if(intervals[i][1]<intervals[j][1]){
-                    intervals[i][1]=intervals[j][1];
-                }
-                j++;
-            }
-            else{
-                ans.push_back(intervals[i]);
-                i=j;
-                j++;
-            }
+vector<vector<int>>ans;
+    int n=intervals.size();
+    sort(intervals.begin(),intervals.end());
+    int i=0,j=1;
+   
+    ans.push_back(intervals[0]);
+    while(j<n){
+        if(ans[i][1]>=intervals[j][0]){
+            ans[i][1]=max(ans[i][1],intervals[j][1]);
         }
-        ans.push_back(intervals[i]);
-        return ans;
+        else{
+            ans.push_back(intervals[j]);
+            i++;
+        }
+        j++;
+    }
+    return ans;
     }
 };
