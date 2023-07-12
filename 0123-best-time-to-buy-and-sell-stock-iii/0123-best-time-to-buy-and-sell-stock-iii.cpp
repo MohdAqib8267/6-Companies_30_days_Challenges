511ccb3e1f0267a930f2ef4c4ddda1cf5ee56c13
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-12 15:01:41 +0530

    Time: 274 ms (67.74%), Space: 88.7 MB (67.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-   int maxProfit(vector<int>& prices) {
-       int min_price1=INT_MAX;
-        int profit1=0;
-          int min_price2=INT_MAX;
-        int profit2=0;
-        for(int i=0;i<prices.size();i++){
-            min_price1=min(min_price1,prices[i]);
-            profit1=max(profit1,prices[i]-min_price1);
-            min_price2=min(min_price2,prices[i]-profit1);
-            profit2=max(profit2,prices[i]-min_price2);
-        }
-        return profit2;
-    }
+   // int maxProfit(vector<int>& prices) {
+   //     int min_price1=INT_MAX;
+   //      int profit1=0;
+   //       int min_price2=INT_MAX;
+   //      int profit2=0;
+   //      for(int i=0;i<prices.size();i++){
+   //          min_price1=min(min_price1,prices[i]);
+   //          profit1=max(profit1,prices[i]-min_price1);
+   //          min_price2=min(min_price2,prices[i]-profit1);
+   //          profit2=max(profit2,prices[i]-min_price2);
+   //      }
+   //      return profit2;
+   //  }
     //-------------------------------------------------------------------->
     //DP
     
@@ -23,57 +23,32 @@ public:
     // initially we have to state = true, b/c we have to buy the stock first
     
     // declare a dp
+    int dp[100000][3][2];
+int solve(int i,int k,bool state,vector<int>& prices,int n){
+    if(i>=n || k<0){
+        return 0;
+    }
+    if(dp[i][k][state]!=-1){
+        return dp[i][k][state];
+    }
     
-//     int dp[100000][3][2];
-    
-//     int helper(vector<int>& prices, int i, int n, int k, bool state)
-//     {
-//         // base case
+    int take=0, not_take=0;
+    if(state){
+        take=-prices[i]+solve(i+1,k-1,!state,prices,n);
+    }
+    else{
+        take=prices[i]+solve(i+1,k,!state,prices,n);
+    }
+    not_take=solve(i+1,k,state,prices,n);
+    return dp[i][k][state]=max(take,not_take);
+}
+    int maxProfit(vector<int>& prices) {
         
-//         if(k < 0 || i == n)
-//             return 0;
+        int n = prices.size();
         
-//         // if already calculated
+        // initialize dp with -1
         
-//         if(dp[i][k][state] != -1)
-//             return dp[i][k][state];
-        
-//         // we have two option either include or exclude
-        
-//         int maxi = INT_MIN;
-        
-//         // inclusion part
-        
-//         // we will be including or excluding depends on state
-        
-//         if(state)
-//         {
-//             maxi = max(maxi, - prices[i] + helper(prices, i + 1, n, k - 1, !state));
-//         }
-//         else
-//         {
-//             maxi = max(maxi, prices[i] + helper(prices, i + 1, n, k, !state));
-//         }
-        
-//         // exclusion part
-        
-//         maxi = max(maxi, helper(prices, i + 1, n, k, state));
-        
-//         // store the res then return
-        
-//         return dp[i][k][state] = maxi;
-//     }
-    
-//     int maxProfit(vector<int>& prices) {
-        
-//         int n = prices.size();
-        
-//         // initialize dp with -1
-        
-//         memset(dp, -1, sizeof(dp));
-        
-//         // call helper
-        
-//         return helper(prices, 0, n, 2, true);
-//     }
+         memset(dp,-1,sizeof(dp));
+            return solve(0,2,true,prices,n);
+    }
 };
