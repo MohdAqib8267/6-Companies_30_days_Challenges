ca03138f06f0dff812cb86deef3920e3f1b37b0b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-13 11:34:41 +0530

    Time: 757 ms (6.44%), Space: 18.1 MB (7.02%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -35,10 +35,27 @@ int solveTab(vector<int>& arr){
     return dp[0];
 
 }
+    
+    //my recursion approach(memoize kre)
+ int solve(int i,vector<int>& arr,int n, vector<int>&dp){
+    if(i>=n-1){
+        return 0;
+    }
+    if(arr[i]==0){
+        return 1e9;;
+    }
+    if(dp[i]!=-1) return dp[i];
+    int ans=1e9;
+    for(int j=i+1;j<=min(arr[i]+i,n-1);j++){
+        ans=min(ans,1+solve(j,arr,n,dp));
+    }
+    return dp[i]=ans;
+}
     int jump(vector<int>& nums) {
-        // int n=nums.size();
-        // vector<int>dp(n+1,-1);
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
         // return solve(0,n,nums,dp);
-        return solveTab(nums);
+        // return solveTab(nums);
+        return solve(0,nums,n,dp);
     }
 };
