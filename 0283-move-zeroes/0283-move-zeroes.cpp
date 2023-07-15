b5fe4e9edb20c94d843767e2029ca50861fc466c
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-15 21:12:09 +0530

    Time: 33 ms (25.19%), Space: 19.3 MB (11.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int cnt=0;
-        int st=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                nums[st++]=nums[i];
-            }
-            else{
-                cnt++;
-            }
-        }
-        while(cnt--){
-            nums[st++]=0;
-        }
-        return;
+    void moveZeroes(vector<int>& arr) {
+       int j=0; //keep maintain non zero element
+	for(int i=0;i<arr.size();i++){
+		if(arr[i]!=0){
+			//that's mean wha per non zero h to j++ kreneg hi
+			swap(arr[i],arr[j]);
+			j++;
+		}
+	}
+	return;
     }
 };
