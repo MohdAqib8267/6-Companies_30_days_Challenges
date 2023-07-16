aabf2638a0868b83e1683f60d0f797baa1e347b6
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-16 21:29:19 +0530

    Time: 17 ms (95.66%), Space: 15.1 MB (12.05%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-   
-		bool cycle(int src, vector<bool> &vis, vector<bool> &stack, vector< int > adj[], vector<int> &ans)
+   bool cycle(int src, vector<bool> &vis, vector<bool> &stack, vector< int > adj[], vector<int> &ans)
 		{
 			vis[src] = true;
 			stack[src] = true;
@@ -26,34 +25,27 @@ public:
 
 			return false;
 		}
-    
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-    int n = numCourses;
-		vector<int> adj[n];
-		for (auto x: prerequisites)
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+         int n = numCourses;
+        vector<int>adj[n];
+        	for (auto x: pre)
 		{
 			vector<int> data = x;
 			int a = data[0];
 			int b = data[1];
 			adj[b].push_back(a);
 		}
-
-		vector<int>ans;
-		vector<bool> vis(n, false), stack(n, false);
-
-		for (int i = 0; i < n; i++)
-		{
-			if (!vis[i])
-			{
-				if (cycle(i, vis, stack, adj, ans))
-				{
-					return {};
-				}
-			}
-		}
-
-	reverse(ans.begin(),ans.end());
-
-		return ans;
+        vector<bool>vis(n,false);
+        vector<bool>stack(n,false);
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(cycle(i,vis,stack,adj,ans)){
+                    return {};
+                }
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
