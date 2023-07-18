d1ed15bfabd001ed5eaa13052289145720ca1d2b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-19 03:17:54 +0530

    Time: 4 ms (85.54%), Space: 8 MB (79.89%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    bool helper(int i,string s,set<string>& wordDict,vector<int>& dp){
-    string temp;
-    if(i>=s.size()){
-        return 1;
-    }
-    if(dp[i]!=-1){
-        return dp[i];
-    }
-    
-    for(int j=i;j<s.size();j++){
-        temp+=s[j];
-        if(wordDict.find(temp)!=wordDict.end()){
-                if(helper(j+1,s,wordDict,dp)) return dp[i]=1;
+    bool solve(int i,string &s, set<string>&st,vector<int>& dp){
+        if(i>=s.size()){
+            return 1;
         }
+        if(dp[i]!=-1) return dp[i];
+        string str="";
+        for(int ind=i;ind<=s.size();ind++){
+            str+=s[ind];
+            if(st.find(str)!=st.end()){
+                if(solve(ind+1,s,st,dp)){
+                    return dp[i]=true;
+                }
+            }
+        }
+        return dp[i]=false;
     }
-    return dp[i]=0;
-}
-bool wordBreak(string s, vector<string>& wordDict)
-{
-    set<string>st;
-    
-    vector<int>dp(s.size(),-1);
-    for(auto a:wordDict)
-    st.insert(a);
-    return helper(0,s,st,dp);
-}
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string>st(wordDict.begin(),wordDict.end());
+        vector<int>dp(s.size()+1,-1);
+        return solve(0,s,st,dp);
+    }
 };
