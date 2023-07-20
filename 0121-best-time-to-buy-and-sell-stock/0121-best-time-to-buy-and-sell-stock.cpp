ac0a540a1fc6810158fae70de886418ba42a0391
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-07-20 22:02:56 +0530

    Time: 141 ms (76.56%), Space: 93.3 MB (72.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int n=prices.size();
-	int buy[n];
-	int sell[n];
-	buy[0]=prices[0];
-	for(int i=1;i<n;i++){
-		buy[i]=min(buy[i-1],prices[i]);
-	}
-	sell[n-1]=prices[n-1];
-	for(int i=n-2;i>=0;i--){
-		sell[i]=max(sell[i+1],prices[i]);
-	}
-	int ans=0;
-	for(int i=0;i<n;i++){
-		ans=max(ans,sell[i]-buy[i]);
-	}
-	return ans;
+        //1 method
+	// int n=prices.size();
+	// int buy[n];
+	// int sell[n];
+	// buy[0]=prices[0];
+	// for(int i=1;i<n;i++){
+	// 	buy[i]=min(buy[i-1],prices[i]);
+	// }
+	// sell[n-1]=prices[n-1];
+	// for(int i=n-2;i>=0;i--){
+	// 	sell[i]=max(sell[i+1],prices[i]);
+	// }
+	// int ans=0;
+	// for(int i=0;i<n;i++){
+	// 	ans=max(ans,sell[i]-buy[i]);
+	// }
+	// return ans;
+    
+        //2nd method
+        int minVal=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.size();i++){
+            profit=max(profit,prices[i]-minVal);
+            minVal=min(minVal,prices[i]);
+        }
+        return profit;
     }
 };
