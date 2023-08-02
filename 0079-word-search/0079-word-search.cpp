7b63b804d1da495d597de72def99c8aabe1e988d
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-02 18:53:08 +0530

    Time: 871 ms (45.39%), Space: 8.1 MB (38.81%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    bool solve(int i,int j,int m,int n,vector<vector<char>>& board,string word,vector<vector<bool>>& vis,int ind){
-        if(ind==word.size()){
+    bool solve(vector<vector<char>>& board,int i,int j,string word,int l,int m,int n,vector<vector<bool>>& vis){
+        if(l>=word.size()){
             return true;
         }
         if(i<0 || i>=m || j<0 || j>=n || vis[i][j]){
             return false;
         }
-        if(board[i][j]==word[ind]){
-             vis[i][j]=true;
-        bool left=solve(i,j-1,m,n,board,word,vis,ind+1);
-        bool up=solve(i-1,j,m,n,board,word,vis,ind+1);
-        bool right=solve(i,j+1,m,n,board,word,vis,ind+1);
-        bool down=solve(i+1,j,m,n,board,word,vis,ind+1);
-            if(left or right or up or down){
+       if(board[i][j]==word[l]){
+           vis[i][j]=true;
+           bool up=solve(board,i-1,j,word,l+1,m,n,vis);
+            bool left=solve(board,i,j-1,word,l+1,m,n,vis);
+            bool down=solve(board,i+1,j,word,l+1,m,n,vis);
+            bool right=solve(board,i,j+1,word,l+1,m,n,vis);
+            if(up or left or down or right){
                 return true;
             }
-        //     } vis[i][j]=false;
-        // return false;
-        }
+       }
         vis[i][j]=false;
-       return false;
-        
+      return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size();
         int n=board[0].size();
-        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        vector<vector<bool>>vis(m,vector<bool>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(solve(i,j,m,n,board,word,vis,0)){
+                    if(solve(board,i,j,word,0,m,n,vis)){
                         return true;
                     }
                 }
