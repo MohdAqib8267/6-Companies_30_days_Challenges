82f6bdddbcda6089bbc97e62179a18ff234bbc65
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-02 22:51:56 +0530

    Time: 7 ms (86.26%), Space: 7.1 MB (32.26%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        //sliding window
         int n=s.size();
         unordered_map<char,int>mp;
+        int res=0;
         int l=0;
         int r=0;
-        int res=0;
         int maxCount=0;
         while(r<n){
             mp[s[r]]++;
-            //now check maximum freq of char(insted of scan whole map using maxCount variable that keep count maximum frequency character)
-            maxCount=max(maxCount,mp[s[r]]); //give maxCount element in current window
-            //now find current window length and check condition
-            int currWindowLen = r-l+1;
-            if(currWindowLen-maxCount <= k){
-                res=max(res,currWindowLen);
-            }
-            else{
-                //substring condition ko meet nhi krti to shrink the current window
+            maxCount=max(maxCount,mp[s[r]]);
+            int len=r-l+1;
+            if(len-maxCount<=k){
+                res=max(res,len);
+            }else{
                 mp[s[l]]--;
                 l++;
-        
             }
             r++;
         }
