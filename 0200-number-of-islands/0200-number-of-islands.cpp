b13634bb14bc56ff83a2e3fe408761beaa3fd30e
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-04 22:50:42 +0530

    Time: 23 ms (99.20%), Space: 12.2 MB (76.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    void bfs(int i,int j,vector<vector<char>>&grid,int m,int n){
+    void DFS(int i,int j,vector<vector<char>>& grid,int m,int n){
+        if(i<0 or i>=m or j<0 or j>=n or grid[i][j]=='0'){
+            return;
+        }
         grid[i][j]='0';
-         queue<pair<int,int>>q;
-       int dirx[4]={-1,0,1,0};
-        int diry[4]={0,-1,0,1};
-             q.push({i,j});
-            while(!q.empty()){
-                        int sz=q.size();
-                        for(int z=0;z<sz;z++){
-                            auto p=q.front(); q.pop();
-                            int r=p.first, c=p.second;
-                            
-                            for(int k=0;k<4;k++){
-                                int nr=r+dirx[k];
-                                int nc=c+diry[k];
-                                if(nr<m and nc<n and nr>=0 and nc>=0 and grid[nr][nc]=='1'){
-                                    grid[nr][nc]='0';
-                                    q.push({nr,nc});
-                                }
-                            }
-                        }
-                        
-                    }
+        DFS(i-1,j,grid,m,n);
+        DFS(i,j-1,grid,m,n);
+        DFS(i+1,j,grid,m,n);
+        DFS(i,j+1,grid,m,n);
+        
     }
     int numIslands(vector<vector<char>>& grid) {
-        //using BFS
-        
         int m=grid.size();
         int n=grid[0].size();
-     
-        int island=0;
+        int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                   
-                   island++;
-                    bfs(i,j,grid,m,n);
+                    DFS(i,j,grid,m,n);
+                    cnt++;
                 }
             }
         }
-        return island;
+        return cnt;
     }
 };
