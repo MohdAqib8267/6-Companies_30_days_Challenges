4beaca2727302869b5a8a2a53a3d12040e126b68
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-04 19:12:14 +0530

    Time: 258 ms (96.75%), Space: 116.9 MB (81.54%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,37 +1,37 @@
 class MedianFinder {
 public:
-    priority_queue<int,vector<int>>pq_max;
-    priority_queue<int,vector<int>,greater<int>>pq_min;
+    priority_queue<int,vector<int>>mx;
+    priority_queue<int,vector<int>,greater<int>>mn;
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(pq_max.size()==0 || pq_max.top()>num){
-            pq_max.push(num);
+        if(mx.size()==0 || mx.top()>num){
+            mx.push(num);
         }
         else{
-            pq_min.push(num);
+            mn.push(num);
         }
-        
-        if(pq_max.size()>1+pq_min.size()){
-            pq_min.push(pq_max.top());
-            pq_max.pop();
-        }else if(pq_min.size()>1+pq_max.size()){
-            pq_max.push(pq_min.top());
-            pq_min.pop();
+        if(mx.size()>1+mn.size()){
+            mn.push(mx.top());
+            mx.pop();
+        }
+        else if(mn.size()>1+mx.size()){
+            mx.push(mn.top());
+            mn.pop();
         }
     }
     
     double findMedian() {
-        if(pq_max.size()==pq_min.size()){
-            return (double) (pq_max.top()+pq_min.top())/2.0;
+        if(mx.size()==mn.size()){
+            return (double) (mn.top()+mx.top())/2.0;
         }
-        else if(pq_min.size()>pq_max.size()){
-            return (double) pq_min.top();
+        else if(mx.size()>mn.size()){
+            return (double) mx.top();
         }
         else{
-            return (double) pq_max.top();
+            return (double) mn.top();
         }
     }
 };
