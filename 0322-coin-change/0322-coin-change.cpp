2198ce3e9c10e542ec1933195fb753ca76c0fa75
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-05 17:10:50 +0530

    Time: 102 ms (68.04%), Space: 14.4 MB (70.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -16,10 +16,23 @@ public:
         return dp[i][amount]=min(take,notTake);
         
     }
+   
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans= solve(n,coins,dp,amount);
-        return ans==100000?-1:ans;
+        // vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        // int ans= solve(n,coins,dp,amount);
+        // return ans==100000?-1:ans;
+        
+        
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<n;j++){
+                if(i>=coins[j] and dp[i-coins[j]]!=INT_MAX){
+                    dp[i]=min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+        }
+        return dp[amount] != INT_MAX ? dp[amount] : -1;
     }
 };
