aeefa225d8b30c3ba88a7dc52ab7a9539cfb9ebb
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-05 22:29:49 +0530

    Time: 4 ms (29.25%), Space: 7.9 MB (14.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,9 +9,26 @@ public:
         int exc=solve(i+1,n,nums,dp);
         return dp[i] = max(inc,exc);
     }
+    int solveTab(vector<int>& nums){
+         int n=nums.size();
+        vector<int>dp(n+1,0);
+        dp[1]=nums[0];
+        if(n>=2){
+            dp[2]=max(nums[0],nums[1]);
+            for(int i=3;i<=n;i++){
+                int inc=nums[i-1]+dp[i-2];
+                int exc=dp[i-1];
+                dp[i]=max(inc,exc);
+            }
+        }
+        return dp[n];
+        
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(0,n,nums,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(0,n,nums,dp);
+        
+        return solveTab(nums);
     }
 };
