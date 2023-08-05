6a3a787bc646b7fc3ee5c6f5faa77666dd514819
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-05 19:17:13 +0530

    Time: 24 ms (30.21%), Space: 22.3 MB (8.27%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    bool solve(int i,string &s, set<string>&st,vector<int>& dp){
+    bool solve(int i,string &s,set<string>st,vector<int>&dp){
         if(i>=s.size()){
-            return 1;
+            return true;
         }
         if(dp[i]!=-1) return dp[i];
         string str="";
-        for(int ind=i;ind<=s.size();ind++){
-            str+=s[ind];
+        for(int j=i;j<s.size();j++){
+            str+=s[j];
             if(st.find(str)!=st.end()){
-                if(solve(ind+1,s,st,dp)){
-                    return dp[i]=true;
+                if(solve(j+1,s,st,dp)){
+                    return dp[i] = true;
                 }
             }
         }
-        return dp[i]=false;
-    }
-    bool solveTab(string s, vector<string>& wordDict){
-        set<string>st(wordDict.begin(),wordDict.end());
-        int n=s.size();
-        vector<int>dp(n+1,0);
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                if(st.find(s.substr(j,i-j+1))!=st.end()){
-                    if(dp[i+1]){
-                        dp[j]=true;
-                        // i=j;
-                        // break;
-                    }
-                }
-            }
-        }
-        return dp[0];
+        return dp[i]= false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        // set<string>st(wordDict.begin(),wordDict.end());
-        // vector<int>dp(s.size()+1,-1);
-        // return solve(0,s,st,dp);
-        
-        return solveTab(s,wordDict);
+        set<string>st(wordDict.begin(),wordDict.end());
+       vector<int>dp(s.size()+1,-1);
+        return solve(0,s,st,dp);
     }
 };
