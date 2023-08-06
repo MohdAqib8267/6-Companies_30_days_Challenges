a5df2b75fe67663796a1c39e0dce0c5c63460a0a
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-06 13:47:42 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (28.96%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -20,23 +20,22 @@ public:
     }
     int solveTab(string s){
         int n=s.size();
-        vector<int>dp(n+1,1);
-        dp[0]=1;
-        dp[1]=1;
-         for (int i = 2; i <= n; i++) {
-        int one = 0, two = 0;
-
+        vector<int> dp(n + 1, 0);
+    dp[0] = 1; // Empty string has one decoding (base case)
+    
+    for (int i = 1; i <= n; i++) {
+        // If the current character is not '0', it can be decoded as a single digit
         if (s[i - 1] != '0') {
-            one = dp[i - 1];
+            dp[i] += dp[i - 1];
         }
-
-        if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6')) {
-            two = dp[i - 2];
+        
+        // If the current and previous characters form a valid two-digit code
+        if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
+            dp[i] += dp[i - 2];
         }
-
-        dp[i] = one + two;
     }
-        return dp[n];
+    
+    return dp[n];
     }
     int numDecodings(string s) {
         int n=s.size();
