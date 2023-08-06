cb131da05722476ea9b1bf0bb63d04b1a2b9ab4e
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-06 15:00:51 +0530

    Time: 45 ms (95.77%), Space: 48.4 MB (40.30%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
+     int solve(int i,vector<int>& nums,int n,vector<int>& dp){
+        if(i>=n-1){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        if(nums[i]==0){
+            return 10000;
+        }
+        int ans=10000;
+         for(int j=i+1;j<=min(nums[i]+i,n-1);j++){
+        ans=min(ans,1+solve(j,nums,n,dp));
+    }
+        return dp[i]=ans;
+    }
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
+         int n=nums.size();
+        // vector<int>dp(n+1,-1);
+        // // return solve(0,n,nums,dp);
+        // //DP solution
+        // return solve(0,nums,n,dp)==10000?false:true;
+        
         int maxJ=0;
         for(int i=0;i<n;i++){
             if(maxJ<i){
