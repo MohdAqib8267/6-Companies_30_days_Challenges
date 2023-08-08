24b1ba7375f60cb2cab525a316c621642459f5c6
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-08 16:05:02 +0530

    Time: 27 ms (44.42%), Space: 20.9 MB (13.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        //for validate rows and cols
-        map<int,set<char>>mp_row, mp_col;
-        //for validate 3*3 matrices
-        map<pair<int,int>,set<char>>grid;
+        int m=board.size();
+        int n=board[0].size();
+        unordered_map<int,set<char>>row,col;
+         map<pair<int,int>,set<char>>grid;
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char num=board[i][j];
                 if(num=='.'){
                     continue;
                 }
-                //check for rows
-                if(mp_row.find(i)!=mp_row.end()){
-                    //check this num repeat or not
-                    if(mp_row[i].count(num)==1){
+                //check row
+                if(row.find(i)!=row.end()){
+                    //find in perticular rpw
+                    if(row[i].count(num)==1){
                         return false;
                     }
                     else{
-                        mp_row[i].insert(num);
+                        row[i].insert(num);
                     }
                 }else{
-                    mp_row[i].insert(num);
+                    row[i].insert(num);
                 }
-                //check for cols
-                if(mp_col.find(j)!=mp_col.end()){
-                    //check this col present or not
-                    if(mp_col[j].count(num)==1){
+                //check col
+                if(col.find(j)!=col.end()){
+                    //find in perticular rpw
+                    if(col[j].count(num)==1){
                         return false;
-                    }else{
-                        mp_col[j].insert(num);
+                    }
+                    else{
+                        col[j].insert(num);
                     }
                 }else{
-                    mp_col[j].insert(num);
+                    col[j].insert(num);
                 }
-                //check for a perticular 3*3 grid
+                //check in grid
                 if(grid.find({i/3,j/3})!=grid.end()){
                     if(grid[{i/3,j/3}].count(num)==1){
                         return false;
