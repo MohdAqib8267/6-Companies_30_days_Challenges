fe2a4262655b0aca5b1129287a65c13c3b0f987b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-08 15:23:22 +0530

    Time: 91 ms (94.44%), Space: 53.4 MB (70.78%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-   bool isPalindrome(string temp){
-        int j=temp.size()-1;
-       int i=0;
+    bool isPalindrome(string s){
+        int i=0;
+        int j=s.size()-1;
         while(i<=j){
-            if(temp[i]!=temp[j]){
+            if(s[i]!=s[j]){
                 return false;
             }
             i++;
@@ -12,14 +12,13 @@ public:
         }
         return true;
     }
-    void solve(int i,string s,int n,vector<string>& temp,vector<vector<string>>& ans){
+    void solve(int i,string s,int n,vector<string>&temp,vector<vector<string>>&ans){
         if(i>=n){
             ans.push_back(temp);
-           
             return;
         }
         string str="";
-        for(int j=i;j<n;j++){
+        for(int j=i;j<=n;j++){
             str=str+s[j];
             if(isPalindrome(str)){
                 temp.push_back(str);
@@ -31,8 +30,7 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
         vector<string>temp;
-        int n=s.size();
-        solve(0,s,n,temp,ans);
+        solve(0,s,s.size(),temp,ans);
         return ans;
     }
 };
