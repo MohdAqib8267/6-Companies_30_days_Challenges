f5e0130c7dbd49a0dfbc66a6f842a2989743d585
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-08 23:46:54 +0530

    Time: 21 ms (97.82%), Space: 32 MB (23.97%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n=customers.size();
-        int NGS=0;
-        int wSum=0;
-        int maxSum=0;
-        int i=0;
+       int i=0;
         int j=0;
-        while(j<n){
+        int sumofNotGrumpy=0;
+        int windowSum=0;
+        int maxWindowSum=0;
+        //basically hum window (mintues ki) ka sum nikalte jayenge and 
+        //maximise krenge and sum nikalenge pure din me jhan per grumpy nhi hai ab jo grumpy minutes wali window hogi usme jo not grumpy wala hai jo wo do baar count ho gya hoga isliye use minues krenge yhi dhyaan rkhey
+        while(j<grumpy.size()){
             if(grumpy[j]==0){
-                NGS+=customers[j];
+                sumofNotGrumpy += customers[j]; // sum of not grumpies inside and outside the window
             }
             else{
-                wSum+=customers[j];
+                windowSum += customers[j]; // window ke ander grumpy walo ka sum
             }
-                if(j-i+1==minutes){
-                    maxSum=max(maxSum,wSum);
-                    if(grumpy[i]==1){
-                        wSum=wSum-customers[i];
-                    }
-                    
-                    i++;
-                
-               
+            if(j-i+1 == minutes){
+                maxWindowSum=max(maxWindowSum,windowSum);
+                if(grumpy[i]==1){ windowSum -= customers[i]; }
+                i++;
             }
             j++;
         }
-        return NGS+maxSum;
+        return sumofNotGrumpy+maxWindowSum;// sab not grumpies and window ke ander jo grumpy hai unka sum
     }
 };
