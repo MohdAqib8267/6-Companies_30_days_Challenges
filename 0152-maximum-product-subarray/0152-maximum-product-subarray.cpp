2102da3b28df545e87fd0898761218d7e32cce49
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-08-25 12:29:01 +0530

    Time: 5 ms (68.36%), Space: 13.8 MB (63.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-   
-    int maxProduct(vector<int>& arr) {
-        int n=arr.size();
-//         int minVal = arr[0];
-//     int maxVal = arr[0];
-
-//     int maxProduct = arr[0];
-
-//     for (int i = 1; i < n; i++) {
-
-//         // When multiplied by -ve number,
-//         // maxVal becomes minVal
-//         // and minVal becomes maxVal.
-//         if (arr[i] < 0)
-//             swap(maxVal, minVal);
-
-//         // maxVal and minVal stores the
-//         // product of subarray ending at arr[i].
-//         maxVal = max(arr[i], maxVal * arr[i]);
-//         minVal = min(arr[i], minVal * arr[i]);
-
-//         // Max Product of array.
-//         maxProduct = max(maxProduct, maxVal);
-//     }
-
-//     // Return maximum product found in array.
-//     return maxProduct;
-        
-        //second method like kadane algo
-        int prod = 1; int maxi = arr[0];
-        for(int i = 0; i < arr.size(); i++){
-            prod = prod*arr[i];
-            maxi = max(prod, maxi);
-            if(prod == 0) prod = 1;
+    int maxProduct(vector<int>& nums) {
+        int p=1;
+        int maxi=nums[0];
+        for(int i=0;i<nums.size();i++){
+            p=p*nums[i];
+            maxi=max(p,maxi);
+            if(p==0){
+                p=1;
+            }
         }
-        prod = 1;
-        for(int i = arr.size()-1; i >= 0; i--){
-            prod = prod*arr[i];
-            maxi = max(prod, maxi);
-            if(prod == 0) prod = 1;
+        cout<<maxi;
+        p=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            p=p*nums[i];
+            maxi=max(p,maxi);
+            if(p==0){
+                p=1;
+            }
         }
         return maxi;
-
     }
 };
