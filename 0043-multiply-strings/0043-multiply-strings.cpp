e1a6eea3fc2f40b38a6e2dd2594e5f00bb1616e0
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-09 14:50:52 +0530

    Time: 6 ms (62.91%), Space: 6.4 MB (87.00%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -3,30 +3,30 @@ public:
     string multiply(string num1, string num2) {
         int n=num1.size();
         int m=num2.size();
-        if(num1=="0" or num2=="0"){
+        if(num1=="0" || num2=="0"){
             return "0";
         }
         reverse(num1.begin(),num1.end());
         reverse(num2.begin(),num2.end());
         int N=n+m;
-        string answer(N,'0');
+        string ans(N,'0');
         for(int i=0;i<m;i++){
-            int digit1=num2[i]-'0';
+            int digit2=num2[i]-'0';
             for(int j=0;j<n;j++){
-                int digit2=num1[j]-'0';
+                int digit1=num1[j]-'0';
                 int pos=i+j;
-                int carry=answer[pos]-'0';
-                int multiplication=digit1*digit2 + carry;
-                int lastdigit=multiplication % 10;
-                carry= multiplication/10;
-                answer[pos]=lastdigit+'0';
-                answer[pos+1]=answer[pos+1]+carry;
+                int carry=ans[pos]-'0';
+                int mul=digit1*digit2+carry;
+                int ld=mul%10;
+                carry=mul/10;
+                ans[pos]=ld+'0';
+                ans[pos+1]+=carry;
             }
         }
-        if(answer.back()=='0'){
-            answer.pop_back();
+        if(ans.back()=='0'){
+            ans.pop_back();
         }
-        reverse(answer.begin(),answer.end());
-        return answer;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
