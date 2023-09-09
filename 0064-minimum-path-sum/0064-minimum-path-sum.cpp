e301a846850ea0d95db904f22a71dd81b1af66df
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-09 22:57:47 +0530

    Time: 11 ms (36.47%), Space: 10.2 MB (19.65%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -16,10 +16,29 @@ public:
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
-        int m=grid[0].size();
+       int m=grid[0].size();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(0,0,grid,dp);
+//         vector<vector<int>>dp(n,vector<int>(m,-1));
+//         return solve(0,0,grid,dp);
+        
+        //Tabulation
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 and j==0){
+                    dp[i][j]=grid[i][j];
+                }
+                else if(i==0 and j!=0){
+                    dp[i][j]=dp[i][j-1]+grid[i][j];
+                }
+                else if(i!=0 and j==0){
+                    dp[i][j]=dp[i-1][j]+grid[i][j];
+                }
+                else
+                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
         
     }
 };
