f604600f5e4156f950461e84cfeee6a46433d6e2
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-14 00:36:39 +0530

    Time: 6 ms (91.51%), Space: 13.2 MB (55.43%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -21,7 +21,30 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        return solve(0,nums.size(),nums,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(0,nums.size(),nums,dp);
+        
+        //bcz we have can multiple same element
+        //Tabulation
+        //bcz we have can multiple same element
+        unordered_map<int,int>mp; //store freq of every element
+        int maxel=INT_MIN; //find maximum element that to calculate where can earn
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            maxel=max(nums[i],maxel);
+        }
+        vector<int>dp(maxel+1,0);
+        dp[1]=mp[1];
+        for(int i=2;i<=maxel;i++){
+            if(mp.find(i)!=mp.end()){
+                //if i==4 then we will delete 3 can consider below 3
+                dp[i]=max(i*mp[i]+dp[i-2],dp[i-1]);
+            }
+            else{
+                //not take
+                dp[i]=dp[i-1];
+            }
+        }
+        return dp[maxel];
     }
 };
