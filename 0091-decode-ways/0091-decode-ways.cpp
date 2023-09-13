69b1eab443158ce4dfb795360b28e55d173ab8a7
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-13 23:42:03 +0530

    Time: 3 ms (48.54%), Space: 16.2 MB (5.54%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int solve(int i,string s,int n,vector<int>&dp){
+   
+   
+    int solve(int i,int n,string s,vector<int>&dp){
         if(i>=n){
             return 1;
         }
@@ -10,40 +12,16 @@ public:
         if(dp[i]!=-1){
             return dp[i];
         }
-        int one=0;
-        int two=0;
-        if((s[i]=='1' and i+1<n) || (s[i]=='2' and i+1<n and s[i+1]<'7')){
-            two=solve(i+2,s,n,dp);
+        int one=0,two=0;
+        if((s[i]=='1' and i+1<n) || (s[i]=='2' and s[i+1]<='6' and i+1<n)){
+            two=solve(i+2,n,s,dp);
         }
-        one=solve(i+1,s,n,dp);
-        return dp[i]=one+two;
-    }
-    int solveTab(string s){
-        int n=s.size();
-        vector<int> dp(n + 1, 0);
-    dp[0] = 1; // Empty string has one decoding (base case)
-    
-    for (int i = 1; i <= n; i++) {
-        // If the current character is not '0', it can be decoded as a single digit
-        if (s[i - 1] != '0') {
-            dp[i] += dp[i - 1];
-        }
-        
-        // If the current and previous characters form a valid two-digit code
-        if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
-            dp[i] += dp[i - 2];
-        }
-    }
-    
-    return dp[n];
+        one=solve(i+1,n,s,dp);
+        return dp[i] = one+two;
     }
     int numDecodings(string s) {
         int n=s.size();
         vector<int>dp(n+1,-1);
-        if(s[0]=='0'){
-            return 0;
-        }
-        // return solve(0,s,n,dp);
-        return solveTab(s);
+        return solve(0,n,s,dp);
     }
 };
