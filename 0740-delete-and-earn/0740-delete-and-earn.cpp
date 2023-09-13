83846f3de2c3e2f8b2fa89fddc0e137254167225
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-14 00:20:56 +0530

    Time: 7 ms (88.80%), Space: 12.4 MB (71.97%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,54 +1,27 @@
 class Solution {
 public:
-    int solve(int i,vector<int>& nums,int n, vector<int>&dp){
-        if(i>=n){
-            return 0;
-        }
+    int solve(int i,int n,vector<int>& nums,vector<int>& dp){
+        if(i>=n) return 0;
         if(dp[i]!=-1){
             return dp[i];
         }
         int sum=nums[i];
         int idx=i+1;
-        //add into same element in sum 
-        while(idx<n and nums[idx]==nums[i]){
+        while(idx<n and nums[i]==nums[idx]){
             sum+=nums[idx];
             idx++;
         }
-        //delete nums[i]+1 elements, we have been sort the element that's why dont need to delete nums[i]-1 elements
         while(idx<n and nums[idx]-nums[i]==1){
             idx++;
         }
-        //include
-        int include = sum+solve(idx,nums,n,dp);
-        int exclude = solve(i+1,nums,n,dp);
-        return dp[i] = max(include,exclude);
+        int inc=sum+solve(idx,n,nums,dp);
+        int exc=solve(i+1,n,nums,dp);
+        return dp[i] = max(inc,exc);
     }
     int deleteAndEarn(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int n=nums.size();
-        // sort(nums.begin(),nums.end());
-        // vector<int>dp(n+1,-1);
-        // return solve(0,nums,n,dp);
-        
-        //Tabulation
-        //bcz we have can multiple same element
-        unordered_map<int,int>mp; //store freq of every element
-        int maxel=INT_MIN; //find maximum element that to calculate where can earn
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-            maxel=max(nums[i],maxel);
-        }
-        vector<int>dp(maxel+1,0);
-        dp[1]=mp[1];
-        for(int i=2;i<=maxel;i++){
-            if(mp.find(i)!=mp.end()){
-                //if i==4 then we will delete 3 can consider below 3
-                dp[i]=max(i*mp[i]+dp[i-2],dp[i-1]);
-            }
-            else{
-                //not take
-                dp[i]=dp[i-1];
-            }
-        }
-        return dp[maxel];
+        vector<int>dp(n+1,-1);
+        return solve(0,nums.size(),nums,dp);
     }
 };
