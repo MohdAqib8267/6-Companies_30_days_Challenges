8a0514f3fea03a5ca7e46a90b64582008201b863
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-14 12:29:07 +0530

    Time: 327 ms (45.81%), Space: 11 MB (36.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int i,int prev,vector<int>&nums,vector<vector<int>>&dp){
+   int solve(int i,int prev,vector<int>&nums,vector<vector<int>>&dp){
         if(i>=nums.size()){
             return 0;
         }
@@ -16,9 +16,8 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        //   vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         // return solve(0,-1,nums,dp);
-        
         vector<int>dp(n+1,1);
         int ans=1;
         for(int i=1;i<n;i++){
