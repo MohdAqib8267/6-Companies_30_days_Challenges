27629bf01b6bfba89946ecdfcdbd0f59374e63ea
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-28 14:26:11 +0530

    Time: 27 ms (22.95%), Space: 17.6 MB (8.94%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<int>>adj[n];
         //Dijekstra
-         vector<vector<int>> adj[n];
-        for(auto &it : flights)
-        {
+        for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-       queue<vector<int>> q;                       //  {node,dist,stop}
-        q.push({src,0,0});                          // {node,dist,stop}
-        vector<int> dis(n,INT_MAX);                
-        dis[src]=0;  
+        vector<int>dis(n,INT_MAX);
+        dis[src]=0;
+        queue<vector<int>>q;
+        q.push({src,0,0}); //{src,cost,stop}
         while(!q.empty()){
-            int node = q.front()[0];
-            int cost = q.front()[1];
-            int stop = q.front()[2];
-            q.pop();
-            
-            if(stop>k) continue;                     
-            
-            for(auto&it : adj[node])
-            {
-                int nexNode = it[0];                 // nextNode
-                int nexWeight = it[1];               // nextNode Weight
+            auto temp=q.front();
+            int node=temp[0];
+            int cost=temp[1];
+            int stop=temp[2];
+             q.pop();
+            if(stop>k){
+                continue;
+            }
+           
+            for(auto it:adj[node]){
+                int nextNode=it[0];
+                int nextWeight=it[1];
                 
-                if(cost +  nexWeight < dis[nexNode])
-                {
-                    dis[nexNode] = cost + nexWeight;                     
-                    q.push({nexNode, dis[nexNode],stop+1});
+                if(dis[nextNode]>nextWeight+cost){
+                    dis[nextNode]=nextWeight+cost;
+                    q.push({nextNode,dis[nextNode],stop+1});
                 }
             }
         }
-         if(dis[dst]==INT_MAX) return -1;                // if I didn't reached to my dst then return -1
-        else return dis[dst];
+        if(dis[dst]==INT_MAX) return -1;
+        return dis[dst];
     }
 };
