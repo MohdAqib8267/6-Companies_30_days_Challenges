31380a2d3f42fb32d6ecc4a41f54910d25774d27
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-29 18:33:17 +0530

    Time: 3 ms (98.08%), Space: 10.7 MB (7.30%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
-         if (i >= grid.size() || j >= grid[0].size()) {
+    int sum=0;
+    int solve(int i,int j,int m,int n,vector<vector<int>>& grid, vector<vector<int>>&dp){
+        if(i>=m || j>=n){
             return INT_MAX;
         }
-        if (dp[i][j] != -1) {
+        if(i==m-1 and j==n-1){
+            return grid[i][j];
+        }
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if (i == grid.size() - 1 && j == grid[0].size() - 1) {
-            return dp[i][j] = grid[i][j];
-        }
-
-        return dp[i][j] = grid[i][j] + min(solve(i, j + 1,grid, dp),solve( i + 1, j,grid, dp));
-        
+        int b=solve(i+1,j,m,n,grid,dp);
+        int r=solve(i,j+1,m,n,grid,dp);
+        return dp[i][j]=grid[i][j]+min({b,r});
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-       int m=grid[0].size();
-        
-//         vector<vector<int>>dp(n,vector<int>(m,-1));
-//         return solve(0,0,grid,dp);
-        
-        //Tabulation
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 and j==0){
-                    dp[i][j]=grid[i][j];
-                }
-                else if(i==0 and j!=0){
-                    dp[i][j]=dp[i][j-1]+grid[i][j];
-                }
-                else if(i!=0 and j==0){
-                    dp[i][j]=dp[i-1][j]+grid[i][j];
-                }
-                else
-                dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n-1][m-1];
-        
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n,grid,dp);
     }
 };
