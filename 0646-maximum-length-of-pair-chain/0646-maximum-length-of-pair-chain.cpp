8ef17d091d41d2b3ea0a38d46c0f79f6904afa3e
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-09-29 19:34:06 +0530

    Time: 199 ms (33.72%), Space: 23.1 MB (33.79%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -4,17 +4,31 @@ public:
         return a[1]<b[1];
     }
     int findLongestChain(vector<vector<int>>& pairs) {
+        //Greedy Approach
         int n=pairs.size();
-        sort(pairs.begin(),pairs.end(),cmp);
-        int i=0;int j=1;
-        int cnt=1;
-        while(j<n){
-            if(pairs[i][1]<pairs[j][0]){
-                cnt++;
-                i=j;
+        // sort(pairs.begin(),pairs.end(),cmp);
+        // int i=0;int j=1;
+        // int cnt=1;
+        // while(j<n){
+        //     if(pairs[i][1]<pairs[j][0]){
+        //         cnt++;
+        //         i=j;
+        //     }
+        //     j++;
+        // }
+        // return cnt;
+        
+        
+        //DP
+        sort(pairs.begin(),pairs.end());
+        vector<int>dp(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(pairs[i][0]>pairs[j][1]){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
             }
-            j++;
         }
-        return cnt;
+        return dp[n-1];
     }
 };
