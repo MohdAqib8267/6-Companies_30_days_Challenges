fd32413362edfb5c6c0f82fb99bffe236cbb409b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2023-10-10 19:19:27 +0530

    Time: 21 ms (45.23%), Space: 8.3 MB (27.71%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
+        int m=s.size();
+        int n=t.size();
         unordered_map<char,int>freq1;
         unordered_map<char,int>freq2;
-        int ans=INT_MAX;
-        for(int i=0;i<t.size();i++){
-            freq1[t[i]]++;
+        for(auto ch:t){
+            freq2[ch]++;
         }
-        int i=0;
-        int j=0;
         int st=-1;
-        int count=0;
-        while(i<n){
-            if(freq1.find(s[i])!=freq1.end()){
-                if(freq2[s[i]]<freq1[s[i]]){
-                    count++;
+        int i=0,j=0;
+        int cnt=0;
+        int ans=INT_MAX;
+        while(j<m){
+            if(freq2.find(s[j])!=freq2.end()){
+                if(freq1[s[j]]<freq2[s[j]]){
+                     cnt++;
                 }
-                freq2[s[i]]++;
+                freq1[s[j]]++;
             }
-            while(count==t.size()){
-                if(ans>(i-j+1)){
-                    ans=(i-j+1);
-                    st=j;
+            while(t.size()==cnt){
+                
+                if(j-i+1<ans){
+                    ans=j-i+1;
+                    st=i;
                 }
-                if(freq1.find(s[j])!=freq1.end()){
-                    freq2[s[j]]--;
-                    if(freq2[s[j]]<freq1[s[j]]){
-                        count--;
+                if(freq2.find(s[i])!=freq2.end()){
+                     freq1[s[i]]--;
+                    if(freq1[s[i]]<freq2[s[i]]){
+                        cnt--;
                     }
+                   
                 }
-                j++;
+                i++;
             }
-            i++;
+            j++;
         }
-        if(st==-1){
-            return "";
-        }
-        return s.substr(st,ans);
+        if(st!=-1){
+            return s.substr(st,ans);
+        }else
+        return "";
     }
 };
