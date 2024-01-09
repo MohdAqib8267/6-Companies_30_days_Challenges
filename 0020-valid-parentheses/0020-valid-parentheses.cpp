17aeb80cbb7e85239767b661d42efb444b6a6260
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-01-09 22:33:41 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (37.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n=s.size();
+        int n = s.size();
         stack<char>st;
-        bool valid=true;
         for(int i=0;i<n;i++){
-            if(s[i]=='(' or s[i]=='{' or s[i]=='['){
+            if(s[i]=='(' or s[i]=='[' or s[i]=='{'){
                 st.push(s[i]);
             }
             else if(s[i]==')'){
                 if(!st.empty() and st.top()=='('){
                     st.pop();
-                }
-                else{
-                    valid=false;
-                    break;
-                }
-            }
-            else if(s[i]=='}'){
-                if(!st.empty() and st.top()=='{'){
-                    st.pop();
-                }
-                else{
-                    valid=false;
-                    break;
+                }else{
+                    return false;
                 }
             }
             else if(s[i]==']'){
                 if(!st.empty() and st.top()=='['){
                     st.pop();
+                }else{
+                    return false;
                 }
-                else{
-                    valid=false;
-                    break;
+            }else if(s[i]=='}'){
+                if(!st.empty() and st.top()=='{'){
+                    st.pop();
+                }else{
+                    return false;
                 }
             }
         }
         if(!st.empty()){
             return false;
         }
-        return valid;
+        return true;
     }
 };
