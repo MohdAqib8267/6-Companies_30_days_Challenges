f534d68ad3a3cd2c8feb8decc6e6c66a9f3dbba8
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-02-13 23:37:19 +0530

    Time: 8 ms (53.15%), Space: 16.7 MB (25.81%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void solve(int i,int j,int oldCol,int newCol,vector<vector<int>>& image){
-        if(i<0 or i>=image.size() or j<0 or j>=image[0].size()){
-            return;
+    void solve(vector<vector<int>>& image,int i,int j,int &col,int m,int n){
+       
+        int curr=image[i][j];
+        image[i][j]=col;
+        if(i-1>=0 and image[i-1][j]==curr){
+            solve(image,i-1,j,col,m,n);
         }
-        if(image[i][j]!=oldCol){
-            return;
+        if(j-1>=0 and image[i][j-1]==curr){
+            solve(image,i,j-1,col,m,n);
         }
-        image[i][j]=newCol;
-        solve(i,j-1,oldCol,newCol,image);
-        solve(i-1,j,oldCol,newCol,image);
-        solve(i,j+1,oldCol,newCol,image);
-        solve(i+1,j,oldCol,newCol,image);
+        if(i+1<m and image[i+1][j]==curr){
+            solve(image,i+1,j,col,m,n);
+        }
+        if(j+1<n and image[i][j+1]==curr){
+            solve(image,i,j+1,col,m,n);
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int oldCol=image[sr][sc];
-        int newCol=color;
-        if(image[sr][sc]!=newCol){
-             solve(sr,sc,oldCol,newCol,image);
+        int m=image.size();
+        int n=image[0].size();
+        if(image[sr][sc]!=color){
+              solve(image,sr,sc,color,m,n);
         }
+      
         return image;
     }
 };
