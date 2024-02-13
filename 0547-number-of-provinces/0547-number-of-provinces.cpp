625a4919c608ee624a92e2f94bda5fac17550caf
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-02-14 01:27:39 +0530

    Time: 20 ms (73.96%), Space: 18.7 MB (26.88%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    void comp(int src,vector<bool>& vis,vector<int>adj[]){
+    void solve(int src,vector<int>adj[],vector<int>&vis){
         vis[src]=true;
         for(auto it:adj[src]){
             if(!vis[it]){
-                comp(it,vis,adj);
+                solve(it,adj,vis);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
+        //find connected components
         int n=isConnected.size();
         vector<int>adj[n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<isConnected[i].size();j++){
-                if(i!=j and isConnected[i][j]==1){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 and i!=j){
                     adj[i].push_back(j);
-                    adj[j].push_back(i);
                 }
             }
         }
+        //| 0-->1
+        //| 1-->0
+        //| 2
+        //count connected components
+        vector<int>vis(n,false);//
         int cnt=0;
-        vector<bool>vis(n,false);
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                comp(i,vis,adj);
+                solve(i,adj,vis);
                 cnt++;
             }
         }
