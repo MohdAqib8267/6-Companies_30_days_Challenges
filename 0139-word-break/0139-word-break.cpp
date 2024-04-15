8c5f4b026da3f033bb6c8dde5009e58752da7ef5
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-15 19:47:41 +0530

    Time: 26 ms (15.00%), Space: 22.8 MB (9.59%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,60 +1,30 @@
 class Solution {
 public:
-    bool solve(int i,string &s,set<string>st,vector<int>&dp){
-        // if(i>=s.size()){
-        //     return true;
-        // }
-        // if(dp[i]!=-1) return dp[i];
-        // string str="";
-        // for(int j=i;j<s.size();j++){
-        //     str+=s[j];
-        //     if(st.find(str)!=st.end()){
-        //         if(solve(j+1,s,st,dp)){
-        //             return dp[i] = true;
-        //         }
-        //     }
-        // }
-        // return dp[i]= false;
-        
+    bool solve(int i, string &s,   set<string>st, vector<int>&dp){
         if(i>=s.size()){
             return true;
         }
-        if(dp[i]!=-1){
+        if(dp[i] != -1){
             return dp[i];
         }
         string str="";
         for(int j=i;j<s.size();j++){
             str+=s[j];
-            if(st.find(str)!=st.end()){
-                if(solve(j+1,s,st,dp)){
-                   return dp[i]=true;
+            if(st.find(str) != st.end()){
+                if(solve(j+1, s, st, dp)){
+                    // cout<<str<<endl;
+                    return dp[i] = true;
                 }
             }
         }
-        return dp[i]=false;
-    }
-    bool solveTab(string s, vector<string>& wordDict){
-        set<string>st(wordDict.begin(),wordDict.end());
-        int n=s.size();
-        vector<int>dp(s.size()+1,0);
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                if(st.find(s.substr(j,i-j+1))!=st.end()){
-                    if(dp[i+1]){
-                        dp[j]=1;
-                    }
-                }
-            }
-        }
-        return dp[0];
-        
+        return dp[i] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
+         int n = wordDict.size();
+    
         set<string>st(wordDict.begin(),wordDict.end());
-       vector<int>dp(s.size()+1,-1);
-        return solve(0,s,st,dp);
-        // return solveTab(s,wordDict);
-        
+        vector<int>dp(s.size()+1,-1);
+    return solve(0,s,st,dp);
+    
     }
 };
