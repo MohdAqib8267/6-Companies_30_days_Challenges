1ebe83b38aff31543a41acd4b4eb758edfc778f3
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-15 20:04:55 +0530

    Time: 19 ms (25.84%), Space: 17.6 MB (24.16%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -19,12 +19,31 @@ public:
         }
         return dp[i] = false;
     }
+     bool solveTab(string s, vector<string>& wordDict){
+        set<string>st(wordDict.begin(),wordDict.end());
+        int n=s.size();
+        vector<int>dp(s.size()+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                if(st.find(s.substr(j,i-j+1))!=st.end()){
+                    if(dp[i+1]){
+                        dp[j]=1;
+                    }
+                }
+            }
+        }
+        return dp[0];
+        
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
          int n = wordDict.size();
     
         set<string>st(wordDict.begin(),wordDict.end());
         vector<int>dp(s.size()+1,-1);
-    return solve(0,s,st,dp);
+    // return solve(0,s,st,dp);
+        
+        return solveTab(s,wordDict);
     
     }
 };
