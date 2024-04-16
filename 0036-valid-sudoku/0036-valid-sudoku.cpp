17ea924278e7bda525183901192ff6dbbe20edf6
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-16 13:10:12 +0530

    Time: 21 ms (30.77%), Space: 25.3 MB (16.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
+    
     bool isValidSudoku(vector<vector<char>>& board) {
-        int m=board.size();
-        int n=board[0].size();
+        int n=board.size();
         unordered_map<int,set<char>>row,col;
-         map<pair<int,int>,set<char>>grid;
+        map<pair<int,int>,set<char>>grid;
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char num=board[i][j];
-                if(num=='.'){
+                if(num == '.'){
                     continue;
                 }
-                //check row
-                if(row.find(i)!=row.end()){
-                    //find in perticular rpw
+                //check in row
+                if(row.find(i) != row.end()){
                     if(row[i].count(num)==1){
                         return false;
-                    }
-                    else{
+                    }else{
                         row[i].insert(num);
                     }
                 }else{
                     row[i].insert(num);
                 }
                 //check col
-                if(col.find(j)!=col.end()){
-                    //find in perticular rpw
+                 if(col.find(j) != col.end()){
                     if(col[j].count(num)==1){
                         return false;
-                    }
-                    else{
+                    }else{
                         col[j].insert(num);
                     }
                 }else{
                     col[j].insert(num);
                 }
+                
                 //check in grid
-                if(grid.find({i/3,j/3})!=grid.end()){
-                    if(grid[{i/3,j/3}].count(num)==1){
+                if(grid.find({i/3,j/3}) != grid.end()){
+                     if(grid[{i/3,j/3}].count(num)==1){
                         return false;
                     }else{
                         grid[{i/3,j/3}].insert(num);
