675137127f96890fc81fe54c15523c40959b4faf
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-21 11:57:25 +0530

    Time: 23 ms (7.94%), Space: 124 MB (5.15%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string countAndSay(int n) {
-           if(n==1){
+        if(n==1){
             return "1";
         }
         string ans=countAndSay(n-1);
-       string res="";
-        
+        //"1" & n==2
+        int cnt=1;
+        string str="";
         for(int i=0;i<ans.size();i++){
-            int cnt=1;
-           while(ans[i]==ans[i+1] and i<ans.size()){
-               cnt++;
-               i++;
-           }
-            res=res+to_string(cnt)+ans[i];
+            while(ans[i] == ans[i+1]){
+                cnt++;
+                i++;
+            }
+            str=str+to_string(cnt)+ans[i];
+            cnt=1;
         }
-        return res;
-
+        return str;
+        
     }
 };
