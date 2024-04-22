9f2691c3bf14b087b16ad3b108cb6d83f58f011b
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-22 15:15:25 +0530

    Time: 23 ms (88.32%), Space: 15.8 MB (57.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
-    
-//     void DFS(int i,int j,vector<vector<char>>& grid,int m,int n){
-//         if(i<0 or i>=m or j<0 or j>=n or grid[i][j]=='0'){
-//             return;
-//         }
-//         grid[i][j]='0';
-//         DFS(i-1,j,grid,m,n);
-//         DFS(i,j-1,grid,m,n);
-//         DFS(i+1,j,grid,m,n);
-//         DFS(i,j+1,grid,m,n);
-        
-//     }
-    
-    void bfs(int i,int j,vector<vector<char>>& grid,int m,int n){
-        int dirx[4]={-1,0,1,0};
-        int diry[4]={0,-1,0,1};
-        grid[i][j]='0';
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        while(!q.empty()){
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                auto p=q.front(); q.pop();
-                int r=p.first, c=p.second;
-                for(int s=0;s<4;s++){
-                    int nr=r+dirx[s];
-                    int nc=c+diry[s];
-                     if(nr<m and nc<n and nr>=0 and nc>=0 and grid[nr][nc]=='1'){
-                                        grid[nr][nc]='0';
-                                        q.push({nr,nc});
-                    }
-                }
-            }
+    void dfs(int i,int j,vector<vector<char>>& grid,int n,int m){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0'){
+            return;
         }
+        grid[i][j]='0';
+        dfs(i-1,j,grid,n,m);
+        dfs(i,j-1,grid,n,m);
+        dfs(i+1,j,grid,n,m);
+        dfs(i,j+1,grid,n,m);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
         int cnt=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
-                    // DFS(i,j,grid,m,n);
-                    bfs(i,j,grid,m,n);
+                    dfs(i,j,grid,n,m);
                     cnt++;
                 }
             }
