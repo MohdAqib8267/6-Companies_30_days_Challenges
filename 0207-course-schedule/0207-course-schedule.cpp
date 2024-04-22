1374e0331328cc8b8967a6b2396992b3584128db
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-22 13:25:09 +0530

    Time: 12 ms (84.77%), Space: 16.5 MB (95.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -23,13 +23,47 @@ public:
             adj[b].push_back(a);
         }
         //if cycle present then we can schedule
-        vector<int>vis(numCourses,0);
-        vector<int>stack(numCourses,0);
+        
+        //DFS
+        // vector<int>vis(numCourses,0);
+        // vector<int>stack(numCourses,0);
+        // for(int i=0;i<numCourses;i++){
+        //     if(isCycle(i,adj,vis,stack)){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+          // BFS 0-->1-->2  [[1,0],[2,1]]  0-->1
+                  // |   |                  // 1-->2  2-->3 3-->4 4-->1
+                  // 4<-- 3
+        vector<int>indeg(numCourses,0);
+        queue<int>q;
+       for(int i=0;i<numCourses;i++){
+           for(auto x:adj[i]){
+               indeg[x]++;
+           }
+       }
         for(int i=0;i<numCourses;i++){
-            if(isCycle(i,adj,vis,stack)){
-                return false;
+            if(indeg[i]==0){
+                q.push(i);
             }
         }
-        return true;
+        int cnt=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            cnt++;
+            for(auto it:adj[node]){
+               indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+       if(cnt==numCourses){
+           return true;
+       }
+        return false;
     }
 };
