d5a3762e538b4f2f4cc58d10b65df93b392423f2
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-04-23 12:30:14 +0530

    Time: 244 ms (8.45%), Space: 72 MB (35.51%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        //Dijekstra
-        //set ya min heap same kaam krenge
-       vector<pair<int,double>>adj[n];
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<pair<int,double>>adj[n];
         for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+            adj[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
-    priority_queue<pair<int,double>,vector<pair<int,double>>,greater<pair<int,double>>>pq;
+        //Dijekstra
+        //set or meanheap
+        priority_queue<pair<int,double>,vector<pair<int,double>>,greater<pair<int,double>>>pq;
+        pq.push({start_node,1});
         vector<double>dist(n,0);
-        dist[start]=1;
-        pq.push({start,1});
+        dist[start_node]=1;
+        
         while(!pq.empty()){
             auto p=pq.top();
             pq.pop();
             int curr_node=p.first;
             double curr_wt=p.second;
             for(auto it:adj[curr_node]){
-                int next_node=it.first;
-                double next_wt=it.second;
+                int next_node = it.first;
+                double next_wt = it.second;
                 if(dist[next_node]<dist[curr_node]*next_wt){
                     dist[next_node]=dist[curr_node]*next_wt;
                     pq.push({next_node,dist[next_node]});
                 }
             }
         }
-        return dist[end];
+        return dist[end_node];
     }
 };
