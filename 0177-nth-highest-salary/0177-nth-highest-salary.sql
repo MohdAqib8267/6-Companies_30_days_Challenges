69a35747f700c07adbac6c6fd50cef31a08d3975
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-06-12 12:15:43 +0530

    Time: 576 ms (91.11%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,10 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N = N - 1;
+    SET N = N - 1;
   RETURN (
-      # Write your MySQL query statement below.
-      select DISTINCT e.salary as getNthHighestSalary from Employee as e
-      order by e.salary desc
-      limit 1 offset N
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+     limit 1 offset N
+      
   );
 END
