e4eab7a27200fc60b017170f4495f6051e527f07
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-06-26 01:44:13 +0530

    Time: 8 ms (50.08%), Space: 18.3 MB (43.93%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -13,29 +13,31 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         queue<pair<TreeNode*,int>>q;
-        if(root==NULL) return 0;
-        int ans=0;
-      
         q.push({root,0});
+        if(root==NULL){
+            return 0;
+        }
+        int ans=0;
         while(!q.empty()){
             int sz=q.size();
             int first,last;
             for(int i=0;i<sz;i++){
-                auto p = q.front();
+                auto p=q.front();
                 q.pop();
                 TreeNode* node=p.first;
-                int curr=p.second;
+                int pos=p.second;
                 if(i==0){
-                    first=curr;
+                    first=pos;
                 }
                 if(i==sz-1){
-                    last=curr;
+                    last=pos;
                 }
-                if(node->left)
-                    q.push({node->left,(long long)2*curr+1});
-                
-                if(node->right)
-                    q.push({node->right,(long long)2*curr+2});
+                if(node->left){
+                    q.push({node->left,(long long)2*pos+1});
+                }
+                if(node->right){
+                    q.push({node->right,(long long)2*pos+2});
+                }
             }
             ans=max(ans,last-first+1);
         }
