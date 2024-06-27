c98848ddb762d2af6e589e327b305035779f413f
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-06-27 15:36:55 +0530

    Time: 3 ms (64.73%), Space: 10.1 MB (79.76%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-   //globally declare arrays
-string one[19]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+   string ones[19]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
 
-string ten[8]={"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-   
+string tens[8]={"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
     string helper(int n){
-       if(n>=1000000000) return helper(n/1000000000)+" Billion"+helper(n%1000000000);
-    
-    if(n>=1000000) return helper(n/1000000)+" Million"+helper(n%1000000);
-    
-    if(n>=1000) return helper(n/1000)+" Thousand"+helper(n%1000);
-    
-    if(n>=100) return helper(n/100)+" Hundred"+helper(n%100);
-    
-    if(n>=20) return " "+ten[n/10-2]+helper(n%10);
-    
-    if(n>=1) return " "+one[n-1];
-    
-    return "";
+        if(n>=1000000000){
+            return helper(n/1000000000)+" Billion"+helper(n%1000000000);
+        }
+        if(n>=1000000){
+            return helper(n/1000000)+" Million"+helper(n%1000000);
+        }
+        if(n>=1000){
+            return helper(n/1000)+" Thousand"+helper(n%1000);
+        }
+        if(n>=100){
+            return helper(n/100)+" Hundred"+helper(n%100);
+        }
+        if(n>=20){
+            return " "+tens[n/10-2]+helper(n%10);
+        }
+        if(n>=1){
+            return " "+ones[n-1];
+        }
+        return "";
     }
     string numberToWords(int num) {
-       if (num == 0) 
+        if(num==0){
             return "Zero";
-        
-        string ans= helper(num);
+        }
+        string ans=helper(num);
         if(ans.back()==' '){
             ans.pop_back();
         }
         return ans.substr(1);
-    };
+    }
 };
