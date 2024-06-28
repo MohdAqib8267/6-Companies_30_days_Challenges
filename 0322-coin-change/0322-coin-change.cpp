6200a3e42fb25868719ca6061042f0cef54b89d3
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-06-28 16:11:47 +0530

    Time: 32 ms (61.24%), Space: 50.9 MB (13.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    int solve(int i,vector<int>&coins,vector<vector<int>>&dp,int amount){
-        if(i<=0 or amount<0){
-            return 100000;
+    int solve(int i,vector<int>& coins, int amount,vector<vector<int>>&dp){
+        if(amount==0){
+            return 0;
         }
-        if(amount==0) return 0;
+        if(i>=coins.size() || amount<0){
+            return 10000;
+        }
         if(dp[i][amount]!=-1){
             return dp[i][amount];
         }
-        int take=100000;
-        if(amount>=coins[i-1]){
-            take=1+solve(i,coins,dp,amount-coins[i-1]);
+        int take=10000,not_take=10000;
+        if(coins[i]<=amount){
+            take=1+solve(i,coins,amount-coins[i],dp);
         }
-       int notTake=solve(i-1,coins,dp,amount);
-        return dp[i][amount]=min(take,notTake);
-        
+        not_take = solve(i+1,coins,amount,dp);
+        return dp[i][amount]=min(take,not_take);
     }
-   
     int coinChange(vector<int>& coins, int amount) {
+        // vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        // int ans=solve(0,coins,amount,dp);
+        // return ans==10000?-1:ans;
         int n=coins.size();
-        // vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        // int ans= solve(n,coins,dp,amount);
-        // return ans==100000?-1:ans;
-        
-        
-        vector<int>dp(amount+1,INT_MAX);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<n;j++){
-                if(i>=coins[j] and dp[i-coins[j]]!=INT_MAX){
-                    dp[i]=min(dp[i],1+dp[i-coins[j]]);
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,10000 + 1));
+        //base case: amount==0
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                
+                if(coins[i-1]>j){
+                    //not take
+                    dp[i][j]=dp[i-1][j];
                 }
+                else{
+                    //take
+                    dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+                }
+                
             }
         }
-        return dp[amount] != INT_MAX ? dp[amount] : -1;
+        return dp[n][amount]==10000 + 1?-1:dp[n][amount];
     }
 };
