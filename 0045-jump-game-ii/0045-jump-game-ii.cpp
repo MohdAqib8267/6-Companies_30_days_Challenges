f7ab31ab52a495b438f77686b8cc7f438df2b0fe
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-06-29 14:19:59 +0530

    Time: 203 ms (11.58%), Space: 20.7 MB (7.63%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,81 +1,21 @@
 class Solution {
 public:
-   int solve(int i,int n,vector<int>& nums,vector<int>& dp){
-        if(i>=n-1){
+    int solve(int i,vector<int>&nums,int n,vector<int>&dp){
+        if(i==n-1){
             return 0;
         }
         if(dp[i]!=-1){
             return dp[i];
         }
-        if(nums[i]==0){
-            return 10000;
-        }
-        int ans=10000;
-        for(int j=1;j<=nums[i];j++){
-            ans=min(ans,1+solve(i+j,n,nums,dp));
+        int ans=1e9+7;
+       for(int j=i+1;j<=min(nums[i]+i,n-1);j++){
+            ans=min(ans,1+solve(j,nums,n,dp));
         }
         return dp[i]=ans;
     }
-int solveTab(vector<int>& arr){
-    int n=arr.size();
-      vector<int> dp(n, INT_MAX);
-
-    dp[n - 1] = 0;
-
-    for (int i = n - 2; i >= 0; i--)
-    {
-        for (int j = i + 1; j <= min(n - 1, i + arr[i]); j++)
-        {
-            if (dp[j] != INT_MAX)
-            {
-                dp[i] = min(dp[i], 1 + dp[j]);
-            }
-        }
-    }
-    return dp[0];
-
-}
-    
-    //my recursion approach(memoize kre)
- int solve(int i,vector<int>& arr,int n, vector<int>&dp){
-    if(i>=n-1){
-        return 0;
-    }
-    if(arr[i]==0){
-        return 1e9;;
-    }
-    if(dp[i]!=-1) return dp[i];
-    int ans=1e9;
-    for(int j=i+1;j<=min(arr[i]+i,n-1);j++){
-        ans=min(ans,1+solve(j,arr,n,dp));
-    }
-    return dp[i]=ans;
-}
-    //Greedy Approach
-    int greedy(vector<int>& nums){
-    int n=nums.size();
-    if(n==1){
-        return 0;
-    }
-    int maxi=nums[0];
-    int curr=maxi;
-    int ans=1;
-    for(int i=1;i<n-1;i++){
-        maxi=max(maxi,i+nums[i]);
-        if(i==curr){
-            ans++;
-            curr=maxi;
-        }
-    }
-    return ans;
-}
     int jump(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n+1,-1);
-        // return solve(0,n,nums,dp);
-        // return solveTab(nums);
-        // return solve(0,nums,n,dp);
-        
-        return greedy(nums);
+        return solve(0,nums,n,dp);
     }
 };
