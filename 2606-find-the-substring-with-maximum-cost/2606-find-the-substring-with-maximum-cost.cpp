2dbcd05d3d4644d77dc389284500a43053c24e37
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-07-07 20:15:08 +0530

    Time: 38 ms (56.51%), Space: 36.8 MB (48.65%) - LeetHub

--- 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.cpp
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-    int findVal(string &str,unordered_map<char,int>&mp){
-        int i=0;
-        int cost=0;
-        while(i<str.size()){
-            if(mp.find(str[i])!=mp.end()){
-                cost=cost+mp[str[i]];
-            }
-            else{
-                cost=cost+((str[i]-'a')+1);
-            }
-        }
-        return cost;
-    }
-    int solve(int i,string s,unordered_map<char,int>&mp){
-        if(i>=s.size()){
-            return 0;
-        }
-        int maxi=0;
-        for(int j=i;j<s.size();j++){
-            string str=s.substr(i,j);
-            int val=findVal(str,mp);
-            maxi=max(maxi,val+solve(j+1,s,mp));
-        }
-        return maxi;
-    }
+    
     int maximumCostSubstring(string s, string chars, vector<int>& vals) {
         unordered_map<char,long long>mp;
         for(int i=0;i<chars.size();i++){
