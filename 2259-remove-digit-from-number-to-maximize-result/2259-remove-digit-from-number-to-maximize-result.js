d4f09029f3fdfc48cc6e4809e233023452d86866
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-07-21 18:12:09 +0530

    Time: 52 ms (70.05%), Space: 51.3 MB (22.58%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
@@ -1,14 +1,18 @@
-class Solution {
-public:
-    string removeDigit(string number, char digit) {
-        string mx="";
-        
-        for(int i=0;i<number.size();i++){
-            if(number[i] == digit){
-                string str = number.substr(0,i)+number.substr(i+1);
-                mx=max(mx,str);
-            }
+/**
+ * @param {string} number
+ * @param {character} digit
+ * @return {string}
+ */
+var removeDigit = function(number, digit) {
+    let mx = 0n; // Initialize mx as a BigInt since we'll be comparing BigInt values
+
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] == digit) {
+            let temp = number.slice(0, i) + number.slice(i + 1);
+           let tempBigInt = BigInt(temp);
+            mx = mx > tempBigInt ? mx : tempBigInt;
         }
-        return mx;
     }
+
+    return mx.toString();
 };
