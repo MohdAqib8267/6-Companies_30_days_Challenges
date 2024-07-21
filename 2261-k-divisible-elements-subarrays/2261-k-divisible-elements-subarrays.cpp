db0970d7c82b03946f9876844b75f0d365959cf8
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-07-21 20:52:01 +0530

    Time: 1382 ms (6.48%), Space: 381.5 MB (11.51%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int countDistinct(vector<int>& nums, int k, int p) {
-        set<vector<int>>s;
-        for(int i=0;i<nums.size();i++){
-            vector<int>temp;
-            int count=0;
-            for(int j=i;j<nums.size();j++){
-                if(nums[j]%p == 0){
-                    count++;
+   int countDistinct(vector<int>& nums, int k, int p) {
+        int l = 0, count = 0;
+        int n = nums.size();
+       set<vector<int>> res;
+
+        for (int r = 0; r < n; ++r) {
+            if (nums[r] % p == 0) {
+                ++count;
+            }
+            
+            while (l <= r && count > k) {
+                if (nums[l] % p == 0) {
+                    --count;
                 }
-                if(count>k){
+                ++l;
+            }
+            
+            for (int i = r - l; i >= 0; --i) {
+                vector<int> subarray(nums.begin() + (r - i), nums.begin() + (r + 1));
+                if (res.find(subarray) == res.end()) {
+                    res.insert(subarray);
+                } else {
                     break;
                 }
-                temp.push_back(nums[j]);
-                s.insert(temp);
             }
         }
-        return s.size();
+
+        return res.size();
     }
 };
