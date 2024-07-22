d91aa2136c150e9c9b2706d91f4f5666fd4a95ad
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-07-22 15:55:59 +0530

    Time: 31 ms (9.52%), Space: 19.4 MB (38.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -9,6 +9,19 @@ public:
             }
         }
     }
+    void BFS(queue<int>&q,vector<bool>&vis,vector<int>adj[]){
+        while(!q.empty()){
+            int src = q.front();
+            q.pop();
+            vis[src]=true;
+            for(auto it:adj[src]){
+                if(!vis[it]){
+                  q.push(it);  
+                }
+            }
+        }
+        
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         vector<int>adj[n];
@@ -22,9 +35,20 @@ public:
         }
         vector<bool>vis(n,false);
         int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]){
+        //         DFS(i,vis,adj);
+        //         cnt++;
+        //     }
+        // }
+        
+        
+        //BFS
+        queue<int>q;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                DFS(i,vis,adj);
+                q.push(i);
+                BFS(q,vis,adj);
                 cnt++;
             }
         }
