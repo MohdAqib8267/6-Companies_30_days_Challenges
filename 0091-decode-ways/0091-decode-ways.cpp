efee846dacbc29e050e74143d5101419bca552ea
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-07-24 14:00:37 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (62.48%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-   
-   
-    int solve(int i,int n,string s,vector<int>&dp){
-        if(i>=n){
+    int solve(int i,int n,string &s, vector<int>&dp){
+        if(i==n){
             return 1;
         }
-        if(s[i]=='0'){
-            return 0;
+        if(s[i]=='0') return 0;
+        if(dp[i] != -1) return dp[i];
+        int two=0;
+        if(i+1<n){
+            if(s[i]=='1' || (s[i]=='2' && s[i+1]<='6')){
+                two=solve(i+2,n,s,dp);
+            }
         }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int one=0,two=0;
-        if((s[i]=='1' and i+1<n) || (s[i]=='2' and s[i+1]<='6' and i+1<n)){
-            two=solve(i+2,n,s,dp);
-        }
-        one=solve(i+1,n,s,dp);
+        int one=solve(i+1,n,s,dp);
         return dp[i] = one+two;
     }
     int numDecodings(string s) {
