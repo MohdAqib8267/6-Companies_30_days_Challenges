37b6d7d80eaa5c921b30795dfc958666c2c17680
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-19 23:42:55 +0530

    Time: 18 ms (28.60%), Space: 7.5 MB (71.95%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -22,8 +22,32 @@ public:
         
         return cnt;
     }
+    int recursion(int step, int val,int copy,int n){
+        if(step>n || val>n){
+            return INT_MAX;
+        }
+        if(val==n){
+            return step;
+        }
+        // return min step to reach the target value
+        // there are 2 choices: paste copied value with current value => 1 step
+        // copy current value and paste it with itself => 2 step
+        int pasteOneStep = recursion(step+1,val+copy, copy, n);
+        int copypasteTwoStep = recursion(step+2,2*val,val,n);
+        return min(pasteOneStep,copypasteTwoStep);
+        
+    }
     int minSteps(int n) {
-       return solve(n);
+       // return solve(n); // Prime number approach
+
+        //DP approach
+        if(n==1){
+            return 0;
+        }
+        //assume that we have copy the number(1 step done) and my value is A
+        // return recursion(1,'A','A',n); //step 1, current Val 'A', copy item 'A',
+        // here we need n, A's so we can show A-->1
+        return recursion(1,1,1,n);
         
     }
 };
