f21a96251e164ce12856478304f7aa587e9ac7b6
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-20 13:45:36 +0530

    Time: 10 ms (82.54%), Space: 12.3 MB (61.29%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int dp[101][202];
-    int solve(int i,vector<int>& piles,int M){
-        if(i>=piles.size()){
-            return 0;
+//     [2,7,9,4] suff=[22,20,13,4]
+    int helper(vector<int>& piles, vector<vector<int>>& dp, const vector<int>& suffixSum, int i, int M) {
+        if (i == piles.size()) return 0; 
+        if (i + 2 * M >= piles.size()) return suffixSum[i]; 
+        if (dp[i][M] != 0) return dp[i][M];
+        int result = 0;
+        for (int x = 1; x <= 2 * M; ++x) {
+            result = max(result, suffixSum[i] - helper(piles, dp, suffixSum, i + x, max(M, x)));
         }
-        if(dp[i][M]!=-1){
-            return dp[i][M];
-        }
-        int tot=0;
-        int ans=INT_MIN;
-        for(int j=0;j<2*M;j++){
-            if(i+j<piles.size()){
-                tot+=piles[i+j];
-            }
-            ans=max(ans,tot-solve(i+j+1,piles,max(M,j+1)));
-        }
-        return dp[i][M]=ans;
+        dp[i][M] = result;
+        return result;
     }
     int stoneGameII(vector<int>& piles) {
-        int sum=0;
-        memset(dp,-1,sizeof(dp));
-        for(int i=0;i<piles.size();i++){
-            sum+=piles[i];
-        }
-        int diff = solve(0,piles,1);
-        //diff hai (a-b); // a -- alice, b--bob 
-        //if we add ((a+b) + (a-b))/2=a(alice)
-        return (sum+diff)/2;
+        if (piles.empty()) return 0;
+        vector<vector<int>> dp(piles.size(), std::vector<int>(piles.size(), 0));
+        vector<int> suffixSum(piles.size());
+        suffixSum[suffixSum.size() - 1] = piles[piles.size() - 1];
+        for (int i = piles.size() - 2; i >= 0; --i) suffixSum[i] = piles[i] + suffixSum[i + 1];
+        return helper(piles, dp, suffixSum, 0, 1);
     }
-    
 };
