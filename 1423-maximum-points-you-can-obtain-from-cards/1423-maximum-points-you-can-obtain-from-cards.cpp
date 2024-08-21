9485959122cb4052dd9f327f6e240bb90fb40188
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-21 17:16:49 +0530

    Time: 46 ms (28.56%), Space: 45 MB (33.03%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int solve(int i,int j,int k,vector<int>& cardPoints, vector<vector<vector<int>>>&dp){
-        if(i>=cardPoints.size() or j<0 or k==0){
+    int solve(int i,int j,int k,vector<int> cardPoints){
+        if(k==0){
             return 0;
         }
-        if(dp[i][j][k]!=-1){
-            return dp[i][j][k];
-        }
-       
-        int left = cardPoints[i]+solve(i+1,j,k-1,cardPoints,dp);
-        int right = cardPoints[j]+solve(i,j-1,k-1,cardPoints,dp);
-        dp[i][j][k] = max(left,right);
-        return dp[i][j][k];
+        if(i==j) return cardPoints[i];
+        if(i>j) return 0;
+        
+        return max(cardPoints[i]+solve(i+1,j,k-1,cardPoints),cardPoints[j]+solve(i,j-1,k-1,cardPoints));
     }
-    
     int maxScore(vector<int>& cardPoints, int k) {
         int n=cardPoints.size();
-        //TLE
-//      vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
-//         return solve(0,n-1,k,cardPoints,dp);
+        // return solve(0,n-1,k,cardPoints); 3D DP gives TLE
         
-        int  sum=0;
-        //calculate sum of first k numbers
+        //Approach 2
+        int sum=0;
         for(int i=0;i<k;i++){
             sum+=cardPoints[i];
         }
