7593e1415b463ef9c8dcfb1247a807ea3431fc01
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-26 16:22:39 +0530

    Time: 121 ms (61.04%), Space: 80.1 MB (16.48%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    
     long long totalCost(vector<int>& costs, int k, int candidates) {
-         priority_queue<long long,vector<long long>,greater<long long>>pq1,pq2;
-        int count=0;
-        long long i=0,j=costs.size()-1;
         long long cost=0;
+        priority_queue<long long,vector<long long>,greater<long long>>pq1,pq2;
+        int count=0;
+        int i=0, j=costs.size()-1;
         while(count<k){
-            while(pq1.size()<candidates and i<=j){
+            while(pq1.size()<candidates && i<=j){
                 pq1.push(costs[i++]);
             }
-            while(pq2.size()<candidates and j>=i){
+            while(pq2.size()<candidates && j>=i){
                 pq2.push(costs[j--]);
             }
-            int a=pq1.size()>0?pq1.top():INT_MAX;
-            int b=pq2.size()>0?pq2.top():INT_MAX;
+            long long a=pq1.size()>0?pq1.top():INT_MAX;
+            long long b=pq2.size()>0?pq2.top():INT_MAX;
             if(a<=b){
-                cost=cost+a;
+                cost+=a;
                 pq1.pop();
-            }
-            else{
-                cost=cost+b;
+            }else{
+                cost+=b;
                 pq2.pop();
             }
             count++;
