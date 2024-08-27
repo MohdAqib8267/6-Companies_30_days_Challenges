dcd30d3633042913d1570453c4a741771e132448
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-27 18:04:22 +0530

    Time: 13 ms (78.24%), Space: 8.4 MB (88.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-   string longestPalindrome(string s) {
+    string longestPalindrome(string s) {
         int n=s.size();
-        int ans=1;
-        int st=0;
-         if(n==1){
-            return s;
-        }
+        if(n==1) return s;
+        int size=0;
+        int st=-1;
         for(int i=0;i<n;i++){
-            int lo=i-1;
-            int hi=i+1;
-            while(lo>=0 and s[i]==s[lo]){
+            int lo=i-1, hi=i+1;
+            while(lo>=0 && s[lo]==s[i]){
                 lo--;
             }
-            while(hi<n and s[i]==s[hi]){
+            while(hi<n && s[i]==s[hi]){
                 hi++;
             }
-            while(lo>=0 and hi<n and s[hi]==s[lo]){
+            while(lo>=0 && hi<n && s[lo]==s[hi]){
                 lo--;
                 hi++;
             }
-            int size=hi-lo-1;
-            if(ans<size){
-                ans=size;
+            if(hi-lo-1 > size){
                 st=lo+1;
+                size = hi-lo-1;
             }
         }
-        return s.substr(st,ans);
+        return s.substr(st,size);
     }
 };
