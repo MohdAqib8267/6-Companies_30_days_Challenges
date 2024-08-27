efe03fb5880d36a1a38b2c62904b0788dc8dd5ce
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-27 12:33:18 +0530

    Time: 1555 ms (6.12%), Space: 410.8 MB (10.03%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,60 +1,29 @@
 class Solution {
 public:
-    bool isPalindrome(int i,int j,string &s){
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            i++;
-            j--;
+   
+    bool isPalindrome(int i,int j,string s){
+        while(i<=j){
+            if(s[i] != s[j]) return false;
+            i++, j--;
         }
         return true;
     }
-    int solve(int i,string s,int n,vector<int>&dp){
-        if(i>=n){
+    int solve(int i,string s,vector<int>&dp){
+        if(i>=s.size()){
             return 0;
         }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int cnt=solve(i+1,s,n,dp);
-        for(int j=i;j<n;j++){
+        if(dp[i] != -1) return dp[i];
+        int cnt=solve(i+1,s,dp);
+        for(int j=i;j<s.size();j++){
             if(isPalindrome(i,j,s)){
-                cnt++; 
+                cnt++;
             }
         }
         return dp[i]=cnt;
     }
     int countSubstrings(string s) {
-        int n=s.size();
-        // vector<int>dp(n+1,-1);
-        // return solve(0,s,n,dp);
+        vector<int>dp(s.size()+1,-1);
+       return solve(0,s,dp);
         
-        // vector<int>dp(n+1,0);
-        // for(int i=n-1;i>=0;i--){
-        //     int cnt=0;
-        //     for(int j=i;j<n;j++){
-        //         if(isPalindrome(i,j,s)){
-        //             cnt++;
-        //         }
-        //     }
-        //     dp[i]=cnt+dp[i+1];
-        // }
-        // return dp[0];
-        
-        //space
-        int next=0;
-        int curr;
-        for(int i=n-1;i>=0;i--){
-            int cnt=0;
-            for(int j=i;j<n;j++){
-                if(isPalindrome(i,j,s)){
-                    cnt++;
-                }
-            }
-             curr=cnt+next;
-            next=curr;
-        }
-        return curr;
     }
 };
