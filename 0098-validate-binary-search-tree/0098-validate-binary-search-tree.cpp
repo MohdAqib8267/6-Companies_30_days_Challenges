2123aa6b9fce5c0e6320e1ddb0b150f2f45dee33
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-30 20:38:17 +0530

    Time: 10 ms (41.29%), Space: 20.3 MB (70.77%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -12,27 +12,24 @@
 class Solution {
 public:
     bool solve(TreeNode* root,TreeNode* mn,TreeNode* mx){
-         if(root==NULL){
+        if(root==NULL){
             return true;
         }
-        if(mx != NULL && root->val >= mx->val){
+        if( mx!=NULL && root->val >= mx->val){
             return false;
         }
-        if(mn != NULL && root->val <= mn->val){
+        if(mn!=NULL && root->val <= mn->val){
             return false;
         }
-        bool left=solve(root->left,mn,root);
-        bool right=solve(root->right,root,mx);
-        if(left && right){
-            return true;
-        }
-        return false;
+        bool left = solve(root->left,mn,root);
+        bool right = solve(root->right,root,mx);
+        return left && right;
     }
     bool isValidBST(TreeNode* root) {
-        TreeNode* mn=NULL;
-        TreeNode* mx=NULL;
-      
-        return solve(root, mn, mx);
-        
+        if(root==NULL){
+            return true;
+        }
+        TreeNode* mn=NULL, *mx=NULL;
+        return solve(root,mn,mx);
     }
 };
