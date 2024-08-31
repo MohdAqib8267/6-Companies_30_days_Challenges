0f688119d720c49c7dfcd7491bc6871930617a15
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-31 16:39:44 +0530

    Time: 14 ms (72.44%), Space: 20 MB (6.91%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-   bool cycle(int src, vector<bool> &vis, vector<bool> &stack, vector< int > adj[], vector<int> &ans)
-		{
-			vis[src] = true;
-			stack[src] = true;
-
-			for (auto x: adj[src])
-			{
-				if (!vis[x])
-				{
-					if (cycle(x, vis, stack, adj, ans))
-					{
-						return true;
-					}
-				}
-				else if (stack[x])
-				{
-					return true;
-				}
-			}
-
-			ans.push_back(src);
-			stack[src] = false;
-
-			return false;
-		}
-    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+    bool isCycle(int src,vector<bool>&vis,vector<bool>&stack,vector<int>adj[],vector<int>&ans){
+        vis[src]=true;
+        stack[src]=true;
+        for(auto it:adj[src]){
+            if(!vis[it]){
+                if(isCycle(it,vis,stack,adj,ans)){
+                    return true;
+                }
+            }else if(stack[it]){
+                return true;
+            }
+        }
+        ans.push_back(src);
+        stack[src]=false;
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
          int n = numCourses;
         vector<int>adj[n];
-        	for (auto x: pre)
+        	for (auto x: prerequisites)
 		{
 			vector<int> data = x;
 			int a = data[0];
 			int b = data[1];
 			adj[b].push_back(a);
 		}
-        vector<bool>vis(n,false);
-        vector<bool>stack(n,false);
+        bool flag=false;
         vector<int>ans;
+        vector<bool>vis(n,false),stack(n,false);
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                if(cycle(i,vis,stack,adj,ans)){
+                
+                if(isCycle(i,vis,stack,adj,ans)){
                     return {};
                 }
             }
