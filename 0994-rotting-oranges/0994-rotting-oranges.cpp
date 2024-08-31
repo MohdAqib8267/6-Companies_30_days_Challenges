3c27c92c958aef2e2369b48d83b13251a8b3fe2f
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-31 12:58:43 +0530

    Time: 0 ms (100.00%), Space: 16.1 MB (80.25%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        int n=grid.size(),m=grid[0].size();
         queue<pair<int,int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                      q.push({i,j});
+                    q.push({i,j});
                 }
             }
         }
-        int time=0;
-        int dirx[4]={-1,0,1,0};
-        int diry[4]={0,-1,0,1};
+        int minutes=0;
+        int dirX[4]={-1,0,1,0};
+        int dirY[4]={0,-1,0,1};
         while(!q.empty()){
             int sz=q.size();
-            for(int z=0;z<sz;z++){
-                auto p=q.front();
+            for(int i=0;i<sz;i++){
+                auto p = q.front();
                 q.pop();
-                int i=p.first, j=p.second;
+                int r=p.first, c=p.second;
                 for(int k=0;k<4;k++){
-                    int ni=i+dirx[k];
-                    int nj=j+diry[k];
-                    if(ni>=0 and nj>=0 and ni<m and nj<n and grid[ni][nj]==1){
-                        grid[ni][nj]=2;
-                        q.push({ni,nj});
+                    int nr=r+dirX[k];
+                    int nc=c+dirY[k];
+                    if(nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc]==1){
+                        grid[nr][nc]=2;
+                        q.push({nr,nc});
                     }
                 }
             }
-            time++;
+            minutes++;
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     return -1;
                 }
             }
         }
-        if(time==0) return 0;
-        return time-1;
+        if(minutes==0) return 0;
+        return minutes-1;
     }
 };
