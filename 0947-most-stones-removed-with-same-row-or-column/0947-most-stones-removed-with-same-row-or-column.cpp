4390152e1650fefd45a2fb293e25f18ca2c3c1f7
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-31 18:15:53 +0530

    Time: 89 ms (37.00%), Space: 18.1 MB (73.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    void DFS(int src,vector<vector<int>>& stones,vector<int>& vis,int n){
-        vis[src]=1;
+    void dfs(int src,vector<vector<int>>& stones,vector<int>&vis,int n){
+        vis[src]=true;
         for(int i=0;i<n;i++){
             if(!vis[i] and (stones[src][0]==stones[i][0] || stones[src][1]==stones[i][1])){
-                DFS(i,stones,vis,n);
+                dfs(i,stones,vis,n);
             }
         }
     }
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        //find, connected components
-        //so given conditions se connected wale component find krenege
-        //ab remove kr payenge (n-cnt)
+       
         vector<int>vis(n,0);
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                DFS(i,stones,vis,n);
-                cnt++;
-            }
+           if(!vis[i]){
+               dfs(i,stones,vis,n);
+               cnt++;
+           }
         }
-        // to jo ek connected component hoga wo apne neighbor ko sbko remove kar payega
-        //sirf ek element backeyga component me baki sab remove me count honge
-        
-        // to short me her component me ek element backeyga
-        // to total elements me se bss us 1 bachey hue ko minus kar do, remove wale aa jayenge
-        return n-cnt; // man lo cnt 1 hi bacha
+        return n-cnt;
     }
 };
