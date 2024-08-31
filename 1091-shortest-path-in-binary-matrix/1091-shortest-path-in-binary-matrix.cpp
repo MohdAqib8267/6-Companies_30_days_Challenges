5f0f5065514c4f34443d62775be5afce247fb56f
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-31 13:32:07 +0530

    Time: 34 ms (96.82%), Space: 22.6 MB (93.43%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -14,29 +14,27 @@ public:
         queue<pair<int,int>>q;
         q.push({0,0});
         grid[0][0]=1;
-        int count=0;
+        int count=1;
         int dirx[8]={-1,-1,0,1,1,1,0,-1};
         int diry[8]={0,1,1,1,0,-1,-1,-1};
         while(!q.empty()){
-            count++;
             int sz=q.size();
             for(int i=0;i<sz;i++){
-                auto p=q.front();
-                q.pop();
-                r=p.first;
-                c=p.second;
+                auto p = q.front(); q.pop();
+                int r=p.first, c=p.second;
                 if(r==m-1 and c==n-1){
                     return count;
                 }
-                //traverse all 8 directions
-                for(int i=0;i<8;i++){
-                    if(r+dirx[i]<m and r+dirx[i]>=0 and c+diry[i]<n and c+diry[i]>=0 and grid[r+dirx[i]][c+diry[i]]==0){
-                        q.push({r+dirx[i], c+diry[i]});
-                        grid[r+dirx[i]][c+diry[i]]=1;
+                for(int k=0;k<8;k++){
+                    int nr=r+dirx[k];
+                    int nc=c+diry[k];
+                    if(nr>=0 and nc>=0 and nr<m and nc<n and grid[nr][nc]==0){
+                        grid[nr][nc]=1;
+                        q.push({nr,nc});
                     }
                 }
-                
             }
+            count++;
         }
         return -1;
     }
