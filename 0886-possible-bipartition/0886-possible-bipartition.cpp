6e27dbd8c8a4bb368675e241582ded947e5d1ebc
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-08-31 13:55:20 +0530

    Time: 138 ms (67.74%), Space: 68.9 MB (59.47%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void dfs(int src,vector<int>adj[],vector<int>& vis,vector<int>&col,bool &flag,int curr){
-        vis[src]=1;
+    void dfs(int src,vector<int>adj[],vector<bool>&vis,vector<int>&col,bool &flag,int curr){
+        if(vis[src]) return;
+        vis[src]=true;
         col[src]=curr;
         for(auto it:adj[src]){
-           if(!vis[it]){
-               dfs(it,adj,vis,col,flag,curr xor 1);
-           }
+            if(!vis[it]){
+                dfs(it,adj,vis,col,flag,curr xor 1);
+            }
             else if(vis[it] and col[it]==curr){
                 flag=false;
                 return;
             }
         }
-        
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<int>adj[n+1];
+        
         for(int i=0;i<dislikes.size();i++){
             adj[dislikes[i][0]].push_back(dislikes[i][1]);
             adj[dislikes[i][1]].push_back(dislikes[i][0]);
         }
+        vector<bool>vis(n+1,false);
         vector<int>col(n+1,-1);
-        vector<int>vis(n+1,0);
         bool flag=true;
         for(int i=1;i<=n;i++){
             if(!vis[i]){
