fcd4bc207d06e8dc7372631a80123e3cd1d71129
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-09-01 16:04:50 +0530

    Time: 437 ms (17.21%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-with cte as(
-    select email,count(email) as cnt from Person
-    group by email
-)
-select email as Email from cte 
-where cnt>1
+# with cte as(
+#     select email,count(email) as cnt from Person
+#     group by email
+# )
+# select email as Email from cte 
+# where cnt>1
+
+select Email from Person
+group by Email
+having count(Email)>1
