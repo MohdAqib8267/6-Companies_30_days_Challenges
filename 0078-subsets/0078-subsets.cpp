175847381b2a7088e7e4743618b820138a01a91f
MOHD AQIB <106628860+MohdAqib8267@users.noreply.github.com>
2024-09-06 13:51:02 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (75.18%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -12,12 +12,26 @@ public:
         solve(i+1,nums,temp,s);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>>ans;
+        // set<vector<int>>s;
+        // vector<int>temp;
+        // solve(0,nums,temp,s);
+        // for(auto it:s){
+        //     ans.push_back(it);
+        // }
+        // return ans;
+        
         vector<vector<int>>ans;
-        set<vector<int>>s;
-        vector<int>temp;
-        solve(0,nums,temp,s);
-        for(auto it:s){
-            ans.push_back(it);
+        int n=nums.size();
+        
+        for(int i=0;i<(1<<n);i++){
+            vector<int>temp;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
